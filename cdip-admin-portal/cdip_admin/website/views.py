b9cd83a32451052fb1a4f057f155b069fb8df37b
matt_morrissette <matt.morrissette@wildlifeprotectionsolutions.org>
2020-06-30 09:17:20 -0600

    Cleaned Up Interface and added Auth0 Integration for Log in

--- cdip-admin-portal/cdip_admin/website/views.py
+++ cdip-admin-portal/cdip_admin/website/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+import json
+from django.contrib.auth import logout as log_out
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from urllib.parse import urlencode
 
 from integrations.models import InboundIntegrationType
+
+from organizations.models import UserProfile
 
 
 # Create your views here.
@@ -18,4 +28,60 @@ def date(request):
 def about(request):
     return HttpResponse("I am working on CDIP. Have a nice day!")
 
+
+@login_required
+def complete(request):
+    user = request.user
+    auth0user = user.social_auth.get(provider='auth0')
+    userdata = {
+        'user_id': auth0user.uid,
+        'name': user.first_name,
+        'lastName': user.last_name,
+        'picture': auth0user.extra_data['picture'],
+        'email': auth0user.extra_data['email'],
+    }
+
+    return render(request, 'website/complete.html', {
+        'auth0User': auth0user,
+        'userdata': json.dumps(userdata, indent=4)
+    })
+
+
+def index(request):
+    user = request.user
+    if user.is_authenticated:
+        return redirect(welcome)
+    else:
+        return render(request, 'index.html')
+
+
+def logout(request):
+    log_out(request)
+    return_to = urlencode({'returnTo': request.build_absolute_uri('/')})
+    logout_url = 'https://%s/v2/logout?client_id=%s&%s' % \
+                 (settings.SOCIAL_AUTH_AUTH0_DOMAIN, settings.SOCIAL_AUTH_AUTH0_KEY, return_to)
+    return HttpResponseRedirect(logout_url)
+
+
+@login_required
+def profile(request):
+    user = request.user
+    auth0user = user.social_auth.get(provider='auth0')
+
+    user_profile = []
+
+    for org in user.userprofile.organizations.all():
+        user_profile.append(org.name)
+
+    userdata = {
+        'user_id': auth0user.uid,
+        'name': user.first_name,
+        'picture': auth0user.extra_data['picture'],
+        'organizations': user_profile
+    }
+
+    return render(request, 'website/profile.html', {
+        'auth0User': auth0user,
+        'userdata': json.dumps(userdata, indent=4)
+    })
 
