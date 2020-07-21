9bc3bf17a7c436d0cf7e6f9eb3dad1d735af8d78
mattmorrissette <66746849+mattmorrissette@users.noreply.github.com>
2020-07-21 12:18:52 -0600

    Added Generic End Points for Configuration

--- cdip-admin-portal/cdip_admin/api/serializers.py
+++ cdip-admin-portal/cdip_admin/api/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
 
 from organizations.models import Organization
+from integrations.models import *
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
+        fields = ['id', 'name', 'description']
+
+
+class InboundIntegrationTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InboundIntegrationType
+        fields = ['id', 'name', 'description']
+
+
+class InboundIntegrationConfigurationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InboundIntegrationConfiguration
+        fields = ['id', 'type', 'owner', 'endpoint', 'cursor', 'login', 'password', 'token',
+                  'defaultConfiguration']
+
+
+class OutboundIntegrationTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OutboundIntegrationType
         fields = ['id', 'name', 'description']
