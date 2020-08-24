8581db20af23342ab756de836a94f10cbb41d1a5
mattmorrissette <66746849+mattmorrissette@users.noreply.github.com>
2020-08-24 15:55:03 -0600

    Added Update/Patch to inbound config

--- cdip-admin-portal/cdip_admin/cdip_admin/settings.py
+++ cdip-admin-portal/cdip_admin/cdip_admin/settings.py
@@ -15,16 +15,12 @@ import environ
 from pathlib import Path
 from django.utils.translation import ugettext_lazy as _
 
-
-
 env = environ.Env()
 # reading .env file
 environ.Env.read_env()
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -74,7 +70,6 @@ AUTH0_TENANT = env('AUTH0_TENANT')
 AUTH0_MANAGEMENT_AUDIENCE = env('AUTH0_MANAGEMENT_AUDIENCE')
 AUTH0_MANAGEMENT_CLIENT_ID = env.str('AUTH0_MANAGEMENT_CLIENT_ID')
 
-
 JWT_AUTH = {
     'JWT_PAYLOAD_GET_USERNAME_HANDLER':
         'cdip_admin.utils.jwt_get_username_from_payload_handler',
@@ -85,7 +80,6 @@ JWT_AUTH = {
     'JWT_ISSUER': env('JWT_ISSUER'),
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
 }
-
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
@@ -98,7 +92,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
-
 
 SOCIAL_AUTH_AUTH0_SCOPE = [
     'openid',
@@ -122,7 +115,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-] 
+]
 
 ROOT_URLCONF = 'cdip_admin.urls'
 
@@ -136,6 +129,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                # 'django.contrib.messages.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -144,16 +138,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cdip_admin.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clip-admin-postgres',
+        'USER': 'postgres',
+        'PASSWORD': '***REMOVED***',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
@@ -164,7 +168,6 @@ SWAGGER_SETTINGS = {
         }
     },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -184,7 +187,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -202,7 +204,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
