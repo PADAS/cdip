02bfa05dcba6cc0ef95e9b1bdd8d73c0c4f950bb
rohitc <rohitc@vulcan.com>
2020-09-14 13:53:44 -0700

    initial checkin.

--- cdip-admin-portal/cdip_admin/cdip_admin/settings.py
+++ cdip-admin-portal/cdip_admin/cdip_admin/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['35.192.111.237']
+ALLOWED_HOSTS = [env.str('ALLOWED_HOSTS', default='localhost')]
 
 STATIC_ROOT = '/var/www/static/'
 
