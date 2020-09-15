227c37168bf43b8b4b2eda2b6bf2ce221beedba1
mattmorrissette <66746849+mattmorrissette@users.noreply.github.com>
2020-09-15 16:24:52 -0600

    Updated unit tests for updating device state and devices

--- cdip-admin-portal/cdip_admin/api/utils.py
+++ cdip-admin-portal/cdip_admin/api/utils.py
@@ -9,12 +9,11 @@ logger = logging.getLogger(__name__)
 def update_device_information(config):
     logger.info('Update Device Information')
     for key in config.state:
-        try:
-            device = Device.objects.get(name=key, type_id=config.type__id)
-        except ObjectDoesNotExist:
-            logger.info('Device not found. Create a new one.')
-            device = Device.objects.create(type_id=config.id, name=key, owner_id=config.owner__id,
-                                           outbound_configuration=config.defaultConfiguration)
+        device, created = Device.objects.get_or_create(external_id=key, inbound_configuration=config)
+
+        # TODO: add outbound configurations to the device in some way
+        for item in config.defaultConfiguration.all():
+            device.outbound_configuration.add(item)
 
         logger.info('Update the state of the device stream.')
-        DeviceState.objects.create(device_id=device.name, end_state=config.state[key])
+        DeviceState.objects.create(device_id=device.id, end_state=config.state[key])
