ef628606a08e1ec9dc705ba14b198cd884b9130a
mattmorrissette <66746849+mattmorrissette@users.noreply.github.com>
2020-09-15 12:15:43 -0600

    Added the DeviceState information, added method for updating devices, a few bug fixes

--- cdip-admin-portal/cdip_admin/accounts/urls.py
+++ cdip-admin-portal/cdip_admin/accounts/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('<str:user_id>', views.account_detail, name='account_detail'),
     path('', views.account_list, name='account_list'),
-    path('add', views.account_add, name='account_add'),
+    path('add/', views.account_add, name='account_add'),
     path('update/<str:user_id>', views.account_update, name='account_update'),
     path('profile/add/<str:user_id>', views.account_profile_add, name='account_profile_add'),
     path('profile/update/<str:user_id>', views.account_profile_update, name='account_profile_update'),
