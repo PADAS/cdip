94e718848659cca692f776a4bca42098eceae61e
mattmorrissette <66746849+mattmorrissette@users.noreply.github.com>
2020-09-17 08:19:20 -0600

    Added DeviceStateList, DeviceList, DeviceDetail endpoints.
    Updated the device state add to only create a new record if the cursor has changed

--- cdip-admin-portal/cdip_admin/api/utils.py
+++ cdip-admin-portal/cdip_admin/api/utils.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from django.core.exceptions import ObjectDoesNotExist
 
 from integrations.models import Device, DeviceState
@@ -6,14 +7,14 @@ from integrations.models import Device, DeviceState
 logger = logging.getLogger(__name__)
 
 
-def update_device_information(config):
+def update_device_information(state, config):
     logger.info('Update Device Information')
-    for key in config.state:
+    for key in state:
         device, created = Device.objects.get_or_create(external_id=key, inbound_configuration=config)
 
         # TODO: add outbound configurations to the device in some way
         for item in config.defaultConfiguration.all():
             device.outbound_configuration.add(item)
 
-        logger.info('Update the state of the device stream.')
-        DeviceState.objects.create(device_id=device.id, end_state=config.state[key])
+        logger.info('Update the state of the device stream if it has changed.')
+        DeviceState.objects.get_or_create(device_id=device.id, end_state=state[key])
