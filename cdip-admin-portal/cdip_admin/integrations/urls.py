6ca4920ef65c35b5efcf2f931e3a0fc24f810b07
mattmorrissette <66746849+mattmorrissette@users.noreply.github.com>
2020-09-21 09:23:21 -0600

    Added basic device/device state views

--- cdip-admin-portal/cdip_admin/integrations/urls.py
+++ cdip-admin-portal/cdip_admin/integrations/urls.py
@@ -3,25 +3,29 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('InboundIntegrationType/<uuid:module_id>', views.inbound_integration_type_detail,
+    path('devices/<uuid:module_id>', views.device_detail,
+         name='device_detail'),
+    path('devices', views.DeviceList.as_view(), name='device_list'),
+
+    path('integrations/inbound/type/<uuid:module_id>', views.inbound_integration_type_detail,
          name='inbound_integration_type_detail'),
-    path('InboundIntegrationType', views.InboundIntegrationTypeList.as_view(), name='inbound_integration_type_list'),
+    path('integrations/inbound/type', views.InboundIntegrationTypeList.as_view(), name='inbound_integration_type_list'),
 
-    path('OutboundIntegrationType/<uuid:module_id>', views.outbound_integration_type_detail,
+    path('integartions/outbound/type/<uuid:module_id>', views.outbound_integration_type_detail,
          name='outbound_integration_type_detail'),
-    path('OutboundIntegrationType', views.OutboundIntegrationTypeList.as_view(), name='outbound_integration_type_list'),
+    path('integrations/outbound/type', views.OutboundIntegrationTypeList.as_view(), name='outbound_integration_type_list'),
 
-    path('InboundIntegrationConfiguration', views.InboundIntegrationConfigurationList.as_view(),
+    path('integrations/inbound/configuration', views.InboundIntegrationConfigurationList.as_view(),
          name="inbound_integration_configuration_list"),
-    path('InboundIntegrationConfiguration/<uuid:module_id>', views.inbound_integration_configuration_detail,
+    path('integrations/inbound/configuration/<uuid:module_id>', views.inbound_integration_configuration_detail,
          name="inbound_integration_configuration_detail"),
-    path('InboundIntegrationConfiguration/Add', views.inbound_integration_configuration_add,
+    path('integrations/inbound/configuration/add', views.inbound_integration_configuration_add,
          name="inbound_integration_configuration_add"),
 
-    path('OutboundIntegrationConfiguration', views.OutboundIntegrationConfigurationList.as_view(),
+    path('integrations/outbound/configuration', views.OutboundIntegrationConfigurationList.as_view(),
          name="outbound_integration_configuration_list"),
-    path('OutboundIntegrationConfiguration/<uuid:module_id>', views.outbound_integration_configuration_detail,
+    path('integrations/outbound/configuration/<uuid:module_id>', views.outbound_integration_configuration_detail,
          name="outbound_integration_configuration_detail"),
-    path('OutboundIntegrationConfiguration/Add', views.outbound_integration_configuration_add,
+    path('integrations/outbound/configuration/add', views.outbound_integration_configuration_add,
          name="outbound_integration_configuration_add"),
 ]
