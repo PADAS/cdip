5698a56683f262e6255c9367edae8b58733fdaad
rohitc <rohitc@vulcan.com>
2020-10-18 22:23:46 -0700

    added telegraf sidecar to send metrics to stackdriver

--- cdip-admin-portal/cdip_admin/integrations/admin.py
+++ cdip-admin-portal/cdip_admin/integrations/admin.py
@@ -3,11 +3,23 @@ from django.contrib import admin
 from .models import OutboundIntegrationType, InboundIntegrationType, OutboundIntegrationConfiguration, \
     InboundIntegrationConfiguration, Device, DeviceGroup, DeviceState
 
+from .views import InboundIntegrationConfigurationForm, OutboundIntegrationConfigurationForm
+
 # Register your models here.
 admin.site.register(InboundIntegrationType)
-admin.site.register(InboundIntegrationConfiguration)
 admin.site.register(OutboundIntegrationType)
-admin.site.register(OutboundIntegrationConfiguration)
 admin.site.register(Device)
 admin.site.register(DeviceGroup)
 admin.site.register(DeviceState)
+
+
+@admin.register(InboundIntegrationConfiguration)
+class InboundIntegrationConfiguration(admin.ModelAdmin):
+    readonly_fields = ['id', ]
+    form = InboundIntegrationConfigurationForm
+
+
+@admin.register(OutboundIntegrationConfiguration)
+class OutboundIntegrationConfiguration(admin.ModelAdmin):
+    readonly_fields = ['id', ]
+    form = OutboundIntegrationConfigurationForm
