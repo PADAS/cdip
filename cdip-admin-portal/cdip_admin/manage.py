6d6fa2efc4be1577bea790c68b1cf8d2e955601b
Chris Doehring <chrisdoehring@gmail.com>
2020-11-19 07:09:18 -0800

    Remove some stale code.

--- cdip-admin-portal/cdip_admin/manage.py
+++ cdip-admin-portal/cdip_admin/manage.py
@@ -3,12 +3,7 @@
 import os
 import sys
 
-
 import cdip_admin.logconfiguration
-
-# logging.basicConfig(format='%(asctime)s %(message)s',
-#     datefmt='%m/%d/%Y %I:%M:%S %p',
-#     level=logging.os.environ.get('LOGGING_LEVEL', 'INFO'))
 
 cdip_admin.logconfiguration.init()
 
