0d11b8882571d3a27db1c436c3f153ee769032c1
mattmorrissette <66746849+mattmorrissette@users.noreply.github.com>
2021-03-02 11:12:42 -0700

    Updates for Database changes and DeviceState call
    -Removed call for updating device states from the put on the InboundIntegrationConfiguration
    
    -Commented out the Patch method as it does not currently serve a purpose.
    
    -Changed the update_device_state call to now use the default device group on the inbound config vs the opposite relationship.

--- cdip-admin-portal/cdip_admin/api/utils.py
+++ cdip-admin-portal/cdip_admin/api/utils.py
@@ -5,25 +5,16 @@ from integrations.models import Device, DeviceState, InboundIntegrationConfigura
 logger = logging.getLogger(__name__)
 
 
-def update_device_information(state, config):
-    logger.info('Update Device Information')
-    for key in state:
-        device, created = Device.objects.get_or_create(external_id=key, inbound_configuration=config)
-
-        # # TODO: add outbound configurations to the device in some way
-        # for item in config.defaultConfiguration.all():
-        #     device.outbound_configuration.add(item)
-
-        logger.info('Update the state of the device stream if it has changed.')
-        DeviceState.objects.get_or_create(device_id=device.id, end_state=state[key])
-
-
-def post_device_information(state, config):
+def post_device_information(state: dict, config: InboundIntegrationConfiguration):
     logger.info('Post Device Information')
 
     name = config.type.name + " - Default"
-    device_group, created = DeviceGroup.objects.get_or_create(inbound_configuration=config,
-                                                              defaults=dict(owner_id=config.owner.id, name=name))
+    if config.default_devicegroup:
+        device_group = config.default_devicegroup
+    else:
+        device_group = DeviceGroup.objects.create(owner_id=config.owner.id, name=name)
+        config.default_devicegroup = device_group
+        config.save()
 
     for key in state:
         device, created = Device.objects.get_or_create(external_id=key, inbound_configuration_id=config.id)
