d69b043b89011a5ede74f6609c7baf5dfd5f2eed
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-03-12 09:52:21 -0500

    Device Group Filters
    Adding Filters for DeviceState and DeviceGroup
    Will go back and refactor DeviceState from a Table to a ListView so that in behaves more like DeviceGroup

--- cdip-admin-portal/cdip_admin/integrations/filters.py
+++ cdip-admin-portal/cdip_admin/integrations/filters.py
@@ -1,22 +1,65 @@
 import django_filters
 
-from integrations.models import DeviceState
+from integrations.models import DeviceState, DeviceGroup
 
 
 class DeviceStateFilter(django_filters.FilterSet):
 
+    def get_choices(model, field):
+        choices = []
+        for k in model.objects.values_list(field).distinct():
+            choices.append((k[0], k[0]))
+        return choices
+
     external_id = django_filters.CharFilter(
         field_name='device__external_id',
-        lookup_expr='exact',
+        lookup_expr='icontains',
         label="External ID"
     )
 
+    organization = django_filters.ChoiceFilter(
+        choices=get_choices(DeviceState, 'device__inbound_configuration__owner__name'),
+        field_name='device__inbound_configuration__owner__name',
+        empty_label='All',
+    )
+
+    # organization = django_filters.ModelChoiceFilter(
+    #     field_name='device__inbound_configuration__owner__name',
+    #     queryset=DeviceState.device.get_queryset().model.inbound_configuration.get_queryset().model.owner.get_queryset()
+    # )
+
     inbound_config_type_name = django_filters.CharFilter(
         field_name='device__inbound_configuration__type__name',
-        lookup_expr='contains',
+        lookup_expr='icontains',
         label='Type'
     )
 
     class Meta:
         model = DeviceState
+        fields = ()
+
+
+class DeviceGroupFilter(django_filters.FilterSet):
+
+    def get_choices(model, field):
+        choices = []
+        for k in model.objects.values_list(field).distinct():
+            choices.append((k[0], k[0]))
+        return choices
+
+    device_group = django_filters.CharFilter(
+        field_name='name',
+        lookup_expr='icontains',
+        label='Name'
+    )
+
+
+    organization = django_filters.ChoiceFilter(
+        choices=get_choices(DeviceGroup, 'owner__name'),
+        field_name='owner__name',
+        empty_label='All',
+    )
+
+    class Meta:
+        model = DeviceGroup
         fields = ()
