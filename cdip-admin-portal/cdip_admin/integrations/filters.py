56c074c68a16e60f4995af31843fb765827aee50
Chris Doehring <chrisdoehring@gmail.com>
2021-03-19 08:23:41 -0700

    Add default ordering on Int. models.

--- cdip-admin-portal/cdip_admin/integrations/filters.py
+++ cdip-admin-portal/cdip_admin/integrations/filters.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 import django_filters
 
 from integrations.models import DeviceState, DeviceGroup, Device, Organization, InboundIntegrationType
@@ -8,21 +10,23 @@ class DeviceStateFilter(django_filters.FilterSet):
     external_id = django_filters.CharFilter(
         field_name='device__external_id',
         lookup_expr='icontains',
-        label="External ID"
+        label=_('External ID'),
     )
 
     organization = django_filters.ModelChoiceFilter(
-        queryset = Organization.objects.all(),
+        queryset = Organization.objects.all().order_by('name'),
         field_name='device__inbound_configuration__owner',
         to_field_name='name',
-        empty_label='All Owners',
+        empty_label=_('All Owners'),
+        distinct=True,
     )
 
     inbound_config_type = django_filters.ModelChoiceFilter(
-        queryset=InboundIntegrationType.objects.all(),
+        queryset=InboundIntegrationType.objects.all().order_by('name'),
         field_name='device__inbound_configuration__type',
         to_field_name='name',
-        empty_label='All Types',
+        empty_label=_('All Types'),
+        distinct=True,
     )
 
     class Meta:
@@ -39,10 +43,11 @@ class DeviceGroupFilter(django_filters.FilterSet):
     )
 
     organization = django_filters.ModelChoiceFilter(
-        queryset = Organization.objects.all(),
+        queryset = Organization.objects.all().order_by('name'),
         field_name='owner',
         to_field_name='name',
-        empty_label='All Owners',
+        empty_label=_('All Owners'),
+        distinct=True,
     )
 
     class Meta:
@@ -56,22 +61,24 @@ class DeviceFilter(django_filters.FilterSet):
     external_id = django_filters.CharFilter(
         field_name='external_id',
         lookup_expr='icontains',
-        label="External ID"
+        label=_('External ID')
     )
 
     organization = django_filters.ModelChoiceFilter(
-        queryset = Organization.objects.all(),
+        queryset = Organization.objects.all().order_by('name'),
         field_name='inbound_configuration__owner',
         to_field_name='name',
-        empty_label='All Owners',
+        empty_label=_('All Owners'),
+        distinct=True,
     )
 
     inbound_config_type = django_filters.ModelChoiceFilter(
-        queryset = InboundIntegrationType.objects.all(),
+        queryset = InboundIntegrationType.objects.all().order_by('name'),
         field_name='inbound_configuration__type',
         to_field_name='name',
-        empty_label='All Types',
-    )
+        empty_label=_('All Types'),
+        distinct=True,
+     )
 
     class Meta:
         model = Device
