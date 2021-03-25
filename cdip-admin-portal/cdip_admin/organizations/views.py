7dff642750ba118df18a4e562c8ef8a5bc1365a7
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-03-25 17:59:54 -0400

    cdip-231 Basic Object Filtering
    Organizations View filtered based on groups user is associated with

--- cdip-admin-portal/cdip_admin/organizations/views.py
+++ cdip-admin-portal/cdip_admin/organizations/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import FormView
 
-from integrations.models import DeviceGroup
 from .forms import OrganizationForm
-from .models import Organization, UserProfile
+from .models import Organization
+from accounts.models import AccountProfile
+from core.permissions import IsGlobalAdmin, IsOrganizationAdmin
+from django.views.generic import ListView
 
 
 @permission_required('core.admin')
@@ -26,11 +27,31 @@ def organizations_detail(request, module_id):
     return render(request, "organizations/organizations_detail.html", {"organization": organization})
 
 
-@permission_required('core.admin')
-def organizations_list(request):
-    organizations = Organization.objects.all()
-    return render(request, "organizations/organizations_list.html",
-                  {"organizations": organizations})
+def get_organizations_for_user(user):
+    organizations = []
+    account_profiles = AccountProfile.objects.filter(user_id=user.username)
+    for account in account_profiles:
+        for organization in account.organizations.values_list('name', flat=True):
+            organizations.append(organization)
+    return organizations
+
+
+class OrganizationsListView(ListView):
+    permission_classes = ([IsGlobalAdmin | IsOrganizationAdmin])
+    template_name = 'organizations/organizations_list.html'
+    queryset = Organization.objects.get_queryset().order_by('name')
+    context_object_name = 'organizations'
+
+    def get_queryset(self):
+        qs = super(OrganizationsListView, self).get_queryset()
+        if not self.request.user.groups.values_list('name', flat=True).filter(name='GlobalAdmin').exists():
+            return self.filter_queryset_for_user(qs, self.request.user)
+        else:
+            return qs
+
+    def filter_queryset_for_user(self, qs, user):
+        organizations = get_organizations_for_user(user)
+        return qs.filter(name__in=organizations)
 
 
 @permission_required('core.admin')
