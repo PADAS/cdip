e699b9b7112c10caa8a9e76d661da09b17a6f4b4
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-03-25 17:59:54 -0400

    cdip-231 Basic Object Filtering
    Organizations View filtered based on groups user is associated with

--- cdip-admin-portal/cdip_admin/organizations/urls.py
+++ cdip-admin-portal/cdip_admin/organizations/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
+from organizations.views import *
 
 urlpatterns = [
     path('<uuid:module_id>', views.organizations_detail, name='organizations_detail'),
     path('add/', views.organizations_add, name='organizations_add'),
-    path('', views.organizations_list, name='organizations_list'),
+    path('', OrganizationsListView.as_view(), name='organizations_list'),
     path('update/<uuid:organization_id>', views.organizations_update, name='organizations_update'),
 ]
