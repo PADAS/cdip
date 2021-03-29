52f32efba438fc9d24135e712ea1f46de492f02e
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-03-29 10:07:14 -0400

    CDIP-231-CreatingThroughModel
    In order to have an additional layer of authorization at the organization level, we are adding a column of role to the model that defines the relationship between account profile and organizations

--- cdip-admin-portal/cdip_admin/accounts/models.py
+++ cdip-admin-portal/cdip_admin/accounts/models.py
@@ -4,9 +4,17 @@ from django.db import models
 from organizations.models import Organization
 
 
+class AccountProfileOrganization(models.Model):
+    account_profile = models.ForeignKey('AccountProfile', on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    role = models.CharField(max_length=200, default='viewer')
+
+
 # Create your models here.
 class AccountProfile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user_id = models.CharField(max_length=200)
-    organizations = models.ManyToManyField(Organization)
+    organizations = models.ManyToManyField(Organization, through=AccountProfileOrganization)
+
+
 
