66c393101e00f04a3ec9b6a5d9a18f1ce25eb7d6
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-03-30 16:13:10 -0400

    CDIP-231 Role Based Permissions
    Checking in a working version role based authorization that uses our new model to check the role of the user requesting an update of the organization object to determine if they are an admin for that organization

--- cdip-admin-portal/cdip_admin/organizations/urls.py
+++ cdip-admin-portal/cdip_admin/organizations/urls.py
@@ -4,8 +4,8 @@ from . import views
 from organizations.views import *
 
 urlpatterns = [
-    path('<uuid:module_id>', views.organizations_detail, name='organizations_detail'),
+    path('<uuid:module_id>', OrganizationDetailView.as_view(), name='organizations_detail'),
     path('add/', views.organizations_add, name='organizations_add'),
     path('', OrganizationsListView.as_view(), name='organizations_list'),
-    path('update/<uuid:organization_id>', views.organizations_update, name='organizations_update'),
+    path('update/<uuid:organization_id>', OrganizationUpdateView.as_view(), name='organizations_update'),
 ]
