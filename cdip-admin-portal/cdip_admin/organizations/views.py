446dfc3bd62f8ebd0be5c9b38f6cfbc58877a783
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-03-30 16:13:10 -0400

    CDIP-231 Role Based Permissions
    Checking in a working version role based authorization that uses our new model to check the role of the user requesting an update of the organization object to determine if they are an admin for that organization

--- cdip-admin-portal/cdip_admin/organizations/views.py
+++ cdip-admin-portal/cdip_admin/organizations/views.py
@@ -1,14 +1,18 @@
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from rest_framework import permissions
+from django.core.exceptions import PermissionDenied
 
 from .forms import OrganizationForm
 from .models import Organization
-from accounts.models import AccountProfile
+from accounts.models import AccountProfile, AccountProfileOrganization
 from core.permissions import IsGlobalAdmin, IsOrganizationAdmin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, UpdateView
 
 
-@permission_required('core.admin')
+@permission_required('organizations.add_organization', raise_exception=True)
 def organizations_add(request):
     if request.method == "POST":
         form = OrganizationForm(request.POST)
@@ -20,11 +24,37 @@ def organizations_add(request):
     return render(request, "organizations/organizations_add.html", {"form": form})
 
 
-# Create your views here.
-@permission_required('core.admin')
-def organizations_detail(request, module_id):
-    organization = get_object_or_404(Organization, pk=module_id)
-    return render(request, "organizations/organizations_detail.html", {"organization": organization})
+class OrganizationUpdateView(PermissionRequiredMixin, UpdateView):
+    template_name = 'organizations/organizations_update.html'
+    form_class = OrganizationForm
+    model = Organization
+    permission_required = 'organizations.change_organization'
+
+    def can_update(self, obj):
+        account_profile_id = AccountProfile.objects.only('id').get(user_id=self.request.user.username).id
+        account_organizations = AccountProfileOrganization.objects.filter(accountprofile_id=account_profile_id)
+        role = account_organizations.only('role').get(organization_id=obj.id).role
+        if role == 'admin':
+            return True
+        return False
+
+    def get_object(self):
+        organization = get_object_or_404(Organization, pk=self.kwargs.get("organization_id"))
+        if not self.can_update(organization):
+            raise PermissionDenied
+        return organization
+
+    def get_success_url(self):
+        return reverse('organizations_detail', kwargs={'module_id': self.kwargs.get("organization_id")})
+
+
+class OrganizationDetailView(PermissionRequiredMixin, DetailView):
+    template_name = 'organizations/organizations_detail.html'
+    model = Organization
+    permission_required = 'organizations.view_organization'
+
+    def get_object(self):
+        return get_object_or_404(Organization, pk=self.kwargs.get("module_id"))
 
 
 def get_organizations_for_user(user):
@@ -37,7 +67,6 @@ def get_organizations_for_user(user):
 
 
 class OrganizationsListView(ListView):
-    permission_classes = ([IsGlobalAdmin | IsOrganizationAdmin])
     template_name = 'organizations/organizations_list.html'
     queryset = Organization.objects.get_queryset().order_by('name')
     context_object_name = 'organizations'
@@ -54,12 +83,5 @@ class OrganizationsListView(ListView):
         return qs.filter(name__in=organizations)
 
 
-@permission_required('core.admin')
-def organizations_update(request, organization_id):
-    organization = get_object_or_404(Organization, id=organization_id)
-    form = OrganizationForm(request.POST or None, instance=organization)
-    if form.is_valid():
-        form.save()
-        return redirect("organizations_detail", organization_id)
-    return render(request, "organizations/organizations_update.html", {"form": form})
+
 
