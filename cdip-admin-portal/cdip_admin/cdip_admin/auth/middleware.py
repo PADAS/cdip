6daa2d08433b266d9ae0cad177885f1743a1c028
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-04-19 20:02:58 -0400

    CDIP-231
    Replace keycloak middleware and backend with Oidc now that we have moved the dependency with keycloak out of the admin portal and into kong which will act as our gateway in front of the portal

--- cdip-admin-portal/cdip_admin/cdip_admin/auth/middleware.py
+++ cdip-admin-portal/cdip_admin/cdip_admin/auth/middleware.py
@@ -1,122 +1,129 @@
-import re
+from rest_framework.utils import json
 
+import json
+import base64
 from django.conf import settings
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import AnonymousUser
+from django.contrib import auth
+from django.contrib.auth import load_backend
+from django.contrib.auth.backends import RemoteUserBackend
+from django.core.exceptions import ImproperlyConfigured
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.functional import SimpleLazyObject
 
-# from django_keycloak.models import Realm
-from django_keycloak.auth import get_remote_user
-from cdip_admin.auth.response import HttpResponseNotAuthorized
 
-
-# def get_realm(request):
-#     if not hasattr(request, '_cached_realm'):
-#         request._cached_realm = Realm.objects.first()
-#     return request._cached_realm
-
-
-def get_user(request, origin_user):
-    # Check for the user as set by
-    # django.contrib.auth.middleware.AuthenticationMiddleware
-    if not isinstance(origin_user, AnonymousUser):
-        return origin_user
-
+def get_user(request):
     if not hasattr(request, '_cached_user'):
-        request._cached_user = get_remote_user(request)
+        request._cached_user = auth.get_user(request)
     return request._cached_user
 
 
-class BaseKeycloakMiddleware(MiddlewareMixin):
+class AuthenticationMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        assert hasattr(request, 'session'), (
+            "The Django authentication middleware requires session middleware "
+            "to be installed. Edit your MIDDLEWARE%s setting to insert "
+            "'django.contrib.sessions.middleware.SessionMiddleware' before "
+            "'django.contrib.auth.middleware.AuthenticationMiddleware'."
+        ) % ("_CLASSES" if settings.MIDDLEWARE is None else "")
+        request.user = SimpleLazyObject(lambda: get_user(request))
 
-    set_session_state_cookie = True
+
+class OidcRemoteUserMiddleware(MiddlewareMixin):
+    """
+    Middleware for utilizing Web-server-provided authentication.
+    If request.user is not authenticated, then this middleware attempts to
+    authenticate the username passed in the ``REMOTE_USER`` request header.
+    If authentication is successful, the user is automatically logged in to
+    persist the user in the session.
+    The header used is configurable and defaults to ``REMOTE_USER``.  Subclass
+    this class and change the ``header`` attribute if you need to use a
+    different header.
+    """
+    # Name of request header to grab username from.  This will be the key as
+    # used in the request.META dictionary, i.e. the normalization of headers to
+    # all uppercase and the addition of "HTTP_" prefix apply.
+    header = "HTTP_X_USERINFO"
+    force_logout_if_no_header = True
 
     def process_request(self, request):
-        """
-        Adds Realm to request.
-        :param request: django request
-        """
-        request.realm = SimpleLazyObject(lambda: get_realm(request))
-
-    def process_response(self, request, response):
-
-        if self.set_session_state_cookie:
-            return self.set_session_state_cookie_(request, response)
-
-        return response
-
-    def set_session_state_cookie_(self, request, response):
-
-        if not request.user.is_authenticated \
-                or not hasattr(request.user, 'oidc_profile'):
-            return response
-
-        jwt = request.user.oidc_profile.jwt
-        if not jwt:
-            return response
-
-        cookie_name = getattr(settings, 'KEYCLOAK_SESSION_STATE_COOKIE_NAME',
-                              'session_state')
-
-        # Set a browser readable cookie which expires when the refresh token
-        # expires.
-        response.set_cookie(
-            cookie_name, value=jwt['session_state'],
-            expires=request.user.oidc_profile.refresh_expires_before,
-            httponly=False
-        )
-
-        return response
-
-
-class KeycloakStatelessBearerAuthenticationMiddleware(BaseKeycloakMiddleware):
-
-    set_session_state_cookie = False
-    header_key = "HTTP_AUTHORIZATION"
-
-    def process_request(self, request):
-        """
-        Forces authentication on all requests except the URL's configured in
-        the exempt setting.
-        """
-        super(KeycloakStatelessBearerAuthenticationMiddleware, self)\
-            .process_request(request=request)
-
-        if hasattr(settings, 'KEYCLOAK_BEARER_AUTHENTICATION_EXEMPT_PATHS'):
-            path = request.path_info.lstrip('/')
-
-            if any(re.match(m, path) for m in
-                   settings.KEYCLOAK_BEARER_AUTHENTICATION_EXEMPT_PATHS):
+        # AuthenticationMiddleware is required so that request.user exists.
+        if not hasattr(request, 'user'):
+            raise ImproperlyConfigured(
+                "The Django remote user auth middleware requires the"
+                " authentication middleware to be installed.  Edit your"
+                " MIDDLEWARE setting to insert"
+                " 'django.contrib.auth.middleware.AuthenticationMiddleware'"
+                " before the RemoteUserMiddleware class.")
+        try:
+            user_info = request.META[self.header]
+            if user_info:
+                user_info = base64.b64decode(user_info)
+                user_info = json.loads(user_info)
+                username = user_info['username']
+                print(user_info)
+        except KeyError:
+            # If specified header doesn't exist then remove any existing
+            # authenticated remote-user, or return (leaving request.user set to
+            # AnonymousUser by the AuthenticationMiddleware).
+            if self.force_logout_if_no_header and request.user.is_authenticated:
+                self._remove_invalid_user(request)
+            return
+        # If the user is already authenticated and that user is the user we are
+        # getting passed in the headers, then the correct user is already
+        # persisted in the session and we don't need to continue.
+        if request.user.is_authenticated:
+            if request.user.get_username() == request.user.get_username():
                 return
-
-        if self.header_key not in request.META:
-            return HttpResponseNotAuthorized(
-                attributes={'realm': request.realm.name})
-
-        user = authenticate(
-            request=request,
-            access_token=request.META[self.header_key].split(' ')[1]
-        )
-
-        if user is None:
-            return HttpResponseNotAuthorized(
-                attributes={'realm': request.realm.name})
-        else:
+            else:
+                # An authenticated user is associated with the request, but
+                # it does not match the authorized user in the header.
+                self._remove_invalid_user(request)
+        # We are seeing this user for the first time in this session, attempt
+        # to authenticate the user.
+        user = auth.authenticate(request, user_info=user_info)
+        if user:
+            # User is valid.  Set request.user and persist user in the session
+            # by logging the user in.
             request.user = user
+            auth.login(request, user)
 
-
-class RemoteUserAuthenticationMiddleware(MiddlewareMixin):
-    set_session_state_cookie = False
-
-    def process_request(self, request):
+    def clean_username(self, username, request):
         """
-        Adds user to the request when authorized user is found in the session
-        :param django.http.request.HttpRequest request: django request
+        Allow the backend to clean the username, if the backend defines a
+        clean_username method.
         """
-        origin_user = getattr(request, 'user', None)
+        backend_str = request.session[auth.BACKEND_SESSION_KEY]
+        backend = auth.load_backend(backend_str)
+        try:
+            username = backend.clean_username(username)
+        except AttributeError:  # Backend has no clean_username method.
+            pass
+        return username
 
-        request.user = SimpleLazyObject(lambda: get_user(
-            request,
-            origin_user=origin_user
-        ))
+    def _remove_invalid_user(self, request):
+        """
+        Remove the current authenticated user in the request which is invalid
+        but only if the user is authenticated via the RemoteUserBackend.
+        """
+        try:
+            stored_backend = load_backend(request.session.get(auth.BACKEND_SESSION_KEY, ''))
+        except ImportError:
+            # backend failed to load
+            auth.logout(request)
+        else:
+            if isinstance(stored_backend, RemoteUserBackend):
+                auth.logout(request)
+
+
+class PersistentRemoteUserMiddleware(OidcRemoteUserMiddleware):
+    """
+    Middleware for Web-server provided authentication on logon pages.
+    Like RemoteUserMiddleware but keeps the user authenticated even if
+    the header (``REMOTE_USER``) is not found in the request. Useful
+    for setups when the external authentication via ``REMOTE_USER``
+    is only expected to happen on some "logon" URL and the rest of
+    the application wants to use Django's authentication mechanism.
+    """
+    force_logout_if_no_header = False
+
+
