115a2579d17c3bac53fbf0dfde6339ea52af3d8f
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-04-21 11:35:41 -0400

    CDIP-231
    checking in new IsServiceAccount permissions

--- cdip-admin-portal/cdip_admin/cdip_admin/auth/backends.py
+++ cdip-admin-portal/cdip_admin/cdip_admin/auth/backends.py
@@ -2,16 +2,29 @@ import json
 import base64
 from django.contrib.auth.backends import *
 
+from clients.models import ClientProfile
+
 
 class SimpleUserInfoBackend(ModelBackend):
     def authenticate(self, request, user_info=None, **kwargs):
+        if not request:
+            # unsure why this happens
+            return
         try:
             if not user_info:
                 header = request.META.get('HTTP_X_USERINFO')
                 user_info = self.get_user_info(header=header)
             username = user_info.get('username') if user_info else None
+            client_id = user_info.get('client_id') if user_info else None
             if username:
                 user = UserModel.objects.get(email=username)
+            if client_id:
+                try:
+                    client_profile = ClientProfile.objects.get(client_id=client_id)
+                    if client_profile:
+                        request.session['client_id'] = client_id
+                except ClientProfile.DoesNotExist:
+                    pass
         except UserModel.DoesNotExist:
             # Run the default password hasher once to reduce the timing
             # difference between an existing and a nonexistent user (#20760).
