8038b52ff4395246e8cb0ded9416e3c4682aed59
Chris Doehring <chrisdoehring@gmail.com>
2021-04-20 18:26:58 -0700

    Hide unused client models in admin.

--- cdip-admin-portal/cdip_admin/clients/admin.py
+++ cdip-admin-portal/cdip_admin/clients/admin.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from clients.models import *
 
 admin.site.register(ClientProfile)
-admin.site.register(AuthorizationScope)
-admin.site.register(InboundClientResource)
-admin.site.register(OutboundClientResource)
-admin.site.register(ClientAudienceScope)
-admin.site.register(InboundClientScope)
-admin.site.register(OutboundClientScope)
+# admin.site.register(AuthorizationScope)
+# admin.site.register(InboundClientResource)
+# admin.site.register(OutboundClientResource)
+# admin.site.register(ClientAudienceScope)
+# admin.site.register(InboundClientScope)
+# admin.site.register(OutboundClientScope)
 
