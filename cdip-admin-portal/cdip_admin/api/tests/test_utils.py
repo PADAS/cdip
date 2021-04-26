4d337f714be4aa5fc596464abdee02c16c2ec7d0
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-04-26 11:09:57 -0400

    cdip-231
    covert test_utils test to pytest so we can align on this standard

--- cdip-admin-portal/cdip_admin/api/tests/test_utils.py
+++ cdip-admin-portal/cdip_admin/api/tests/test_utils.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+import pytest
 
 from api.utils import post_device_information
 from integrations.models import Device, DeviceState, InboundIntegrationConfiguration, InboundIntegrationType, \
@@ -7,146 +7,104 @@ from integrations.models import Device, DeviceState, InboundIntegrationConfigura
 from organizations.models import Organization
 
 
-class TestUpdateDeviceInformation(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # This where to setup the initial data
-        organization = Organization.objects.create(name="WPS", description="An Organization for testing.")
+def test_post_device_information(device_group):
+    # Update a Bunch of devices
+    inbound_config = InboundIntegrationConfiguration.objects.first()
+    assert inbound_config is not None
+    post_device_information(inbound_config.state, inbound_config)
+    count = Device.objects.count()
 
-        outbound_type = OutboundIntegrationType.objects.create(name="EarthRanger", description="ER")
+    # Assert the count is correct
+    assert count == 10
 
-        outbound_config = OutboundIntegrationConfiguration.objects.create(type_id=outbound_type.id, owner_id=organization.id)
+    # Assert some of the cursor values in the DeviceState Table
+    device_1 = Device.objects.get(external_id='ST2010-2758')
+    device_state_1 = DeviceState.objects.get(device__id=device_1.id)
+    assert device_state_1.state == 14469583
 
-        inbound_type = InboundIntegrationType.objects.create(name="Savannah Tracker")
+    device_2 = Device.objects.get(external_id='ST2010-2762')
+    device_state_2 = DeviceState.objects.get(device__id=device_2.id)
+    assert device_state_2.state == 14488454
 
-        name = inbound_type.name + " - Default Group"
+    # Update the cursor info to add a few more devices and update a couple of the cursors
+    state = dict({
+        "ST2010-2758": 14469900,
+        "ST2010-2759": 14430249,
+        "ST2010-2760": 14650428,
+        "ST2010-2761": 14722788,
+        "ST2010-2762": 14488750,
+        "ST2010-2763": 14454926,
+        "ST2010-2764": 14699313,
+        "ST2010-2765": 14428313,
+        "ST2010-2766": 14383613,
+        "ST2010-2767": 14174427,
+        "ST2010-2768": 14031055,
+        "ST2010-2769": 14210284,
+        "ST2010-2770": 17351262,
+        "ST2010-2771": 17560037,
+        "ST2010-2772": 17638614
+    })
 
-        device_group = DeviceGroup.objects.create(owner_id=outbound_config.owner.id, name=name)
-        device_group.destinations.add(outbound_config)
+    inbound_config.state = state
+
+    post_device_information(state, inbound_config)
+    count = Device.objects.count()
+    # Assert the count is correct
+    assert count == 15
+
+    # Assert that work check the count and values of a couple devices
+    device_1 = Device.objects.get(external_id='ST2010-2758')
+    device_state_1 = DeviceState.objects.filter(device__id=device_1.id).order_by('-created_at').first()
+    assert device_state_1.state == 14469900
+
+    device_2 = Device.objects.get(external_id='ST2010-2762')
+    device_state_2 = DeviceState.objects.filter(device__id=device_2.id).order_by('-created_at').first()
+    assert device_state_2.state == 14488750
+
+    device_3 = Device.objects.get(external_id='ST2010-2760')
+    device_state_3 = DeviceState.objects.filter(device__id=device_3.id).order_by('-created_at').first()
+    assert device_state_3.state == 14650428
+
+    device_4 = Device.objects.get(external_id='ST2010-2772')
+    device_state_4 = DeviceState.objects.filter(device__id=device_4.id).order_by('-created_at').first()
+    assert device_state_4.state == 17638614
+
+    outbound_config = DeviceGroup.objects.get(devices=device_4).destinations.first()
+    assert outbound_config is not None
+    outbound_type = outbound_config.type
+    assert outbound_type is not None
+    assert outbound_type.name == "EarthRanger"
 
 
-        state = dict({
-            "ST2010-2758": 14469583,
-            "ST2010-2759": 14430249,
-            "ST2010-2760": 14650428,
-            "ST2010-2761": 14722788,
-            "ST2010-2762": 14488454,
-            "ST2010-2763": 14454926,
-            "ST2010-2764": 14699313,
-            "ST2010-2765": 14428313,
-            "ST2010-2766": 14383613,
-            "ST2010-2767": 14174427
-        })
+@pytest.fixture
+def device_group(db, django_user_model):
+    # This where to setup the initial data
+    organization = Organization.objects.create(name="WPS", description="An Organization for testing.")
 
-        inbound_config = InboundIntegrationConfiguration.objects.create(type_id=inbound_type.id, owner_id=organization.id,
-                                                                        state=state, default_devicegroup=device_group)
+    outbound_type = OutboundIntegrationType.objects.create(name="EarthRanger", description="ER")
 
-    def test_post_device_information(self):
-        # Update a Bunch of devices
-        inbound_config = InboundIntegrationConfiguration.objects.first()
-        self.assertIsNotNone(inbound_config)
-        post_device_information(inbound_config.state, inbound_config)
-        count = Device.objects.count()
+    outbound_config = OutboundIntegrationConfiguration.objects.create(type_id=outbound_type.id, owner_id=organization.id)
 
-        # Assert the count is correct
-        self.assertEquals(count, 10)
+    inbound_type = InboundIntegrationType.objects.create(name="Savannah Tracker")
 
-        # Assert some of the cursor values in the DeviceState Table
-        device_1 = Device.objects.get(external_id='ST2010-2758')
-        device_state_1 = DeviceState.objects.get(device__id=device_1.id)
-        self.assertEquals(device_state_1.state, 14469583)
+    name = inbound_type.name + " - Default Group"
 
-        device_2 = Device.objects.get(external_id='ST2010-2762')
-        device_state_2 = DeviceState.objects.get(device__id=device_2.id)
-        self.assertEquals(device_state_2.state, 14488454)
+    device_group = DeviceGroup.objects.create(owner_id=outbound_config.owner.id, name=name)
+    device_group.destinations.add(outbound_config)
 
-        # Update the cursor info to add a few more devices and update a couple of the cursors
-        state = dict({
-            "ST2010-2758": 14469900,
-            "ST2010-2759": 14430249,
-            "ST2010-2760": 14650428,
-            "ST2010-2761": 14722788,
-            "ST2010-2762": 14488750,
-            "ST2010-2763": 14454926,
-            "ST2010-2764": 14699313,
-            "ST2010-2765": 14428313,
-            "ST2010-2766": 14383613,
-            "ST2010-2767": 14174427,
-            "ST2010-2768": 14031055,
-            "ST2010-2769": 14210284,
-            "ST2010-2770": 17351262,
-            "ST2010-2771": 17560037,
-            "ST2010-2772": 17638614
-        })
 
-        inbound_config.state = state
+    state = dict({
+        "ST2010-2758": 14469583,
+        "ST2010-2759": 14430249,
+        "ST2010-2760": 14650428,
+        "ST2010-2761": 14722788,
+        "ST2010-2762": 14488454,
+        "ST2010-2763": 14454926,
+        "ST2010-2764": 14699313,
+        "ST2010-2765": 14428313,
+        "ST2010-2766": 14383613,
+        "ST2010-2767": 14174427
+    })
 
-        post_device_information(state, inbound_config)
-        count = Device.objects.count()
-        # Assert the count is correct
-        self.assertEquals(count, 15)
-
-        # Assert that work check the count and values of a couple devices
-        device_1 = Device.objects.get(external_id='ST2010-2758')
-        device_state_1 = DeviceState.objects.filter(device__id=device_1.id).order_by('-created_at').first()
-        self.assertEquals(device_state_1.state, 14469900)
-
-        device_2 = Device.objects.get(external_id='ST2010-2762')
-        device_state_2 = DeviceState.objects.filter(device__id=device_2.id).order_by('-created_at').first()
-        self.assertEquals(device_state_2.state, 14488750)
-
-        device_3 = Device.objects.get(external_id='ST2010-2760')
-        device_state_3 = DeviceState.objects.filter(device__id=device_3.id).order_by('-created_at').first()
-        self.assertEquals(device_state_3.state, 14650428)
-
-        device_4 = Device.objects.get(external_id='ST2010-2772')
-        device_state_4 = DeviceState.objects.filter(device__id=device_4.id).order_by('-created_at').first()
-        self.assertEquals(device_state_4.state, 17638614)
-
-        outbound_config = DeviceGroup.objects.get(devices=device_4).destinations.first()
-        self.assertIsNotNone(outbound_config)
-        outbound_type = outbound_config.type
-        self.assertIsNotNone(outbound_type)
-        self.assertEquals(outbound_type.name, "EarthRanger")
-
-        # state = dict(cursors={
-        #     "ST2010-2758": 14469583,
-        #     "ST2010-2759": 14430249,
-        #     "ST2010-2760": 14650428,
-        #     "ST2010-2761": 14722788,
-        #     "ST2010-2762": 14488454,
-        #     "ST2010-2763": 14454926,
-        #     "ST2010-2764": 14699313,
-        #     "ST2010-2765": 14428313,
-        #     "ST2010-2766": 14383613,
-        #     "ST2010-2767": 14174427,
-        #     "ST2010-2768": 14031055,
-        #     "ST2010-2769": 14210284,
-        #     "ST2010-2770": 17351262,
-        #     "ST2010-2771": 17560037,
-        #     "ST2010-2772": 17638614,
-        #     "ST2010-3042": 17744801,
-        #     "ST2010-3043": 20746124,
-        #     "ST2010-3044": 20864549,
-        #     "ST2010-3045": 17826618,
-        #     "ST2010-3046": 17726857,
-        #     "ST2010-3047": 17748119,
-        #     "ST2010-3048": 17763270,
-        #     "ST2010-3049": 17755930,
-        #     "ST2010-3050": 17767553,
-        #     "ST2010-3051": 17752117,
-        #     "ST2010-3052": 17767767,
-        #     "ST2010-3053": 17762774,
-        #     "ST2010-3054": 17744395,
-        #     "ST2010-3055": 17793712,
-        #     "ST2010-3056": 17750800,
-        #     "ST2010-3057": 17738471,
-        #     "IRI2016-3285": 31416290,
-        #     "IRI2016-3313": 31446299,
-        #     "IRI2016-3314": 31446394,
-        #     "IRI2016-3315": "20075981",
-        #     "IRI2016-3384": 31440883,
-        #     "IRI2016-3385": "19399113",
-        #     "IRI2016-3386": "19399111",
-        #     "IRI2016-3387": "20041332"
-        # })
+    inbound_config = InboundIntegrationConfiguration.objects.create(type_id=inbound_type.id, owner_id=organization.id,
+                                                                    state=state, default_devicegroup=device_group)
