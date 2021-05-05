0606e5257c32a7f65fc211d823e12b7dd5e62c39
Chris Doehring <chrisdoehring@gmail.com>
2021-05-05 12:52:05 -0700

    Add Bridge. Add widgets.

--- cdip-admin-portal/cdip_admin/integrations/forms.py
+++ cdip-admin-portal/cdip_admin/integrations/forms.py
@@ -5,86 +5,12 @@ from core.permissions import IsGlobalAdmin, IsOrganizationMember
 from organizations.models import Organization
 from .models import OutboundIntegrationConfiguration, OutboundIntegrationType, InboundIntegrationConfiguration, \
     InboundIntegrationType, DeviceGroup, Device, BridgeIntegration
+from core.widgets import FormattedJsonFieldWidget, PeekabooTextInput
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field
+from crispy_forms.bootstrap import (
+    PrependedText, PrependedAppendedText, FormActions)
 
-
-class DeviceGroupForm(forms.ModelForm):
-    class Meta:
-        model = DeviceGroup
-        exclude = ['id', 'devices',]
-
-    def __init__(self, *args, request=None, **kwargs):
-        super(DeviceGroupForm, self).__init__(*args, **kwargs)
-        if self.instance and request:
-            qs = Organization.objects.all()
-            if not IsGlobalAdmin.has_permission(None, request, None):
-                self.fields['owner'].queryset = IsOrganizationMember.\
-                    filter_queryset_for_user(qs, request.user, 'name')
-            else:
-                self.fields['owner'].queryset = qs
-
-
-class DeviceGroupManagementForm(forms.ModelForm):
-    class Meta:
-        model = DeviceGroup
-        exclude = ['id', 'name', 'destinations', 'owner']
-
-
-class InboundIntegrationConfigurationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(render_value=True), required=False)
-
-    class Meta:
-        model = InboundIntegrationConfiguration
-        exclude = ['id',]
-        labels = {'default_devicegroup': "Default Device Group"}
-        widgets = {
-            'password': forms.PasswordInput(),
-            # 'state': forms.HiddenInput()
-        }
-
-    def __init__(self, *args, request=None, **kwargs):
-        super(InboundIntegrationConfigurationForm, self).__init__(*args, **kwargs)
-        if self.instance and request:
-            qs = Organization.objects.all()
-            if not IsGlobalAdmin.has_permission(None, request, None):
-                self.fields['owner'].queryset = IsOrganizationMember. \
-                    filter_queryset_for_user(qs, request.user, 'name', admin_only=True)
-            else:
-                self.fields['owner'].queryset = qs
-
-
-class InboundIntegrationTypeForm(forms.ModelForm):
-
-    class Meta:
-        model = InboundIntegrationType
-        exclude = ['id']
-
-
-class OutboundIntegrationConfigurationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(render_value=True), required=False)
-
-    class Meta:
-        model = OutboundIntegrationConfiguration
-        exclude = ['id']
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
-
-    def __init__(self, *args, request=None, **kwargs):
-        super(OutboundIntegrationConfigurationForm, self).__init__(*args, **kwargs)
-        if self.instance and request:
-            qs = Organization.objects.all()
-            if not IsGlobalAdmin.has_permission(None, request, None):
-                self.fields['owner'].queryset = IsOrganizationMember. \
-                    filter_queryset_for_user(qs, request.user, 'name', admin_only=True)
-            else:
-                self.fields['owner'].queryset = qs
-
-
-class OutboundIntegrationTypeForm(forms.ModelForm):
-
-    class Meta:
-        model = OutboundIntegrationType
-        exclude = ['id']
 from django import forms
 
 from accounts.utils import get_user_profile
@@ -109,27 +35,38 @@ class DeviceGroupForm(forms.ModelForm):
             else:
                 self.fields['owner'].queryset = qs
 
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
+
 
 class DeviceGroupManagementForm(forms.ModelForm):
     class Meta:
         model = DeviceGroup
         exclude = ['id', 'name', 'destinations', 'owner']
 
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
+
 
 class InboundIntegrationConfigurationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(render_value=True), required=False)
 
     class Meta:
         model = InboundIntegrationConfiguration
         exclude = ['id',]
+        fields = (
+            'name', 'type', 'owner', 'enabled', 'default_devicegroup', 'endpoint', 'login', 'password', 'token', 'state'
+        )
         labels = {'default_devicegroup': "Default Device Group"}
         widgets = {
-            'password': forms.PasswordInput(),
-            # 'state': forms.HiddenInput()
+            'password': PeekabooTextInput(),
+            'token': PeekabooTextInput(),
+            'state': FormattedJsonFieldWidget(),
         }
 
     def __init__(self, *args, request=None, **kwargs):
-        super(InboundIntegrationConfigurationForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         if self.instance and request:
             qs = Organization.objects.all()
             if not IsGlobalAdmin.has_permission(None, request, None):
@@ -138,12 +75,20 @@ class InboundIntegrationConfigurationForm(forms.ModelForm):
             else:
                 self.fields['owner'].queryset = qs
 
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
+
 
 class InboundIntegrationTypeForm(forms.ModelForm):
 
     class Meta:
         model = InboundIntegrationType
         exclude = ['id']
+
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
 
 
 class OutboundIntegrationConfigurationForm(forms.ModelForm):
@@ -166,6 +111,10 @@ class OutboundIntegrationConfigurationForm(forms.ModelForm):
             else:
                 self.fields['owner'].queryset = qs
 
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
+
 
 class OutboundIntegrationTypeForm(forms.ModelForm):
 
@@ -173,16 +122,95 @@ class OutboundIntegrationTypeForm(forms.ModelForm):
         model = OutboundIntegrationType
         exclude = ['id']
 
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
 
-class BridgeIntegrationForm(forms.ModelForm):
+
+class DeviceGroupForm(forms.ModelForm):
+    class Meta:
+        model = DeviceGroup
+        exclude = ['id', 'devices',]
+
+    def __init__(self, *args, request=None, **kwargs):
+        super(DeviceGroupForm, self).__init__(*args, **kwargs)
+        if self.instance and request:
+            qs = Organization.objects.all()
+            if not IsGlobalAdmin.has_permission(None, request, None):
+                self.fields['owner'].queryset = IsOrganizationMember.\
+                    filter_queryset_for_user(qs, request.user, 'name')
+            else:
+                self.fields['owner'].queryset = qs
+
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
+
+
+class DeviceGroupManagementForm(forms.ModelForm):
+    class Meta:
+        model = DeviceGroup
+        exclude = ['id', 'name', 'destinations', 'owner']
+
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
+
+
+class InboundIntegrationTypeForm(forms.ModelForm):
+
+    class Meta:
+        model = InboundIntegrationType
+        exclude = ['id']
+
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
+
+
+class OutboundIntegrationConfigurationForm(forms.ModelForm):
     # password = forms.CharField(widget=forms.PasswordInput(render_value=True), required=False)
 
     class Meta:
-        model = BridgeIntegration
+        model = OutboundIntegrationConfiguration
         exclude = ['id']
-        # widgets = {
-        #     'password': forms.PasswordInput(),
-        # }
+        widgets = {
+            'password': PeekabooTextInput(attrs={'class': 'form-control'}),
+            'token': PeekabooTextInput(attrs={'class': 'form-control'}),
+            'state': FormattedJsonFieldWidget(),
+        }
+
+    def __init__(self, *args, request=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and request:
+            qs = Organization.objects.all()
+            if not IsGlobalAdmin.has_permission(None, request, None):
+                self.fields['owner'].queryset = IsOrganizationMember. \
+                    filter_queryset_for_user(qs, request.user, 'name', admin_only=True)
+            else:
+                self.fields['owner'].queryset = qs
+
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
+
+
+class OutboundIntegrationTypeForm(forms.ModelForm):
+
+    class Meta:
+        model = OutboundIntegrationType
+        exclude = ['id']
+
+class BridgeIntegrationForm(forms.ModelForm):
+
+    class Meta:
+        model = BridgeIntegration
+        exclude = ['id', 'state',]
+        fields = ('name', 'type', 'owner', 'enabled', 'additional', 'state')
+        widgets = {
+            'additional': FormattedJsonFieldWidget(),
+            'state': FormattedJsonFieldWidget(),
+        }
 
     def __init__(self, *args, request=None, **kwargs):
         super(BridgeIntegrationForm, self).__init__(*args, **kwargs)
@@ -193,3 +221,7 @@ class BridgeIntegrationForm(forms.ModelForm):
                     filter_queryset_for_user(qs, request.user, 'name', admin_only=True)
             else:
                 self.fields['owner'].queryset = qs
+
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Save', css_class='btn-primary'))
+    helper.form_method = 'POST'
