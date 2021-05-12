fffb888ed203650aa93bf709c3c2516843d859d7
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-05-12 15:38:47 -0400

    cdip-299
    combine the copy to clipboard action into the peakaboo widget html, js and css
    
    refactor some of the javascript and css so that the widgets are independent of one another and dont effect each other
    
    hiding the clipboard when the text is not visible as the copy does not work on the password type

--- cdip-admin-portal/cdip_admin/core/static/widgets/peekaboo/peekaboo.js
+++ cdip-admin-portal/cdip_admin/core/static/widgets/peekaboo/peekaboo.js
@@ -1,14 +1,28 @@
 $(document).ready(function() {
     $("#show_hide_password a").on('click', function(event) {
         event.preventDefault();
-        if($('#show_hide_password input').attr("type") == "text"){
-            $('#show_hide_password input').attr('type', 'password');
-            $('.peakaboo').addClass( "fa-eye-slash" );
-            $('.peakaboo').removeClass( "fa-eye" );
-        }else if($('#show_hide_password input').attr("type") == "password"){
-            $('#show_hide_password input').attr('type', 'text');
-            $('.peakaboo').removeClass( "fa-eye-slash" );
-            $('.peakaboo').addClass( "fa-eye" );
+        let sensative_text = event.target.parentElement.parentElement.querySelector('input')
+        let peakaboo = event.target.parentElement.parentElement.querySelector('.peakaboo')
+        let clip_board = event.target.parentElement.parentElement.querySelector('.copy-to-clip')
+        if($(sensative_text).attr("type") == "text"){
+            $(sensative_text).attr('type', 'password');
+            $(peakaboo).addClass( "fa-eye-slash" );
+            $(peakaboo).removeClass( "fa-eye" );
+            $(clip_board).hide();
+        }else if($(sensative_text).attr("type") == "password"){
+            $(sensative_text).attr('type', 'text');
+            $(peakaboo).removeClass( "fa-eye-slash" );
+            $(peakaboo).addClass( "fa-eye" );
+            $(clip_board).show();
         }
     });
 });
+
+function copyToClipboard(event) {
+    let r = document.createRange();
+    let element = event.target.parentElement.querySelector('input')
+    r.selectNode(element)
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand("copy");
+}
