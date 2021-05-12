6332b8b7a3fe9cbec12d2ecb76cebc633afe1e17
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2021-05-12 15:38:47 -0400

    cdip-299
    combine the copy to clipboard action into the peakaboo widget html, js and css
    
    refactor some of the javascript and css so that the widgets are independent of one another and dont effect each other
    
    hiding the clipboard when the text is not visible as the copy does not work on the password type

--- cdip-admin-portal/cdip_admin/core/widgets.py
+++ cdip-admin-portal/cdip_admin/core/widgets.py
@@ -42,5 +42,4 @@ class PeekabooTextInput(forms.widgets.TextInput):
         css = {
             'all': ('widgets/peekaboo/peekaboo.css',)
         }
-        js = ('widgets/peekaboo/peekaboo.js', 'https://kit.fontawesome.com/0f5032f73b.js',
-              'widgets/copy_to_clipboard/copy_to_clipboard.js')
+        js = ('widgets/peekaboo/peekaboo.js', 'https://kit.fontawesome.com/0f5032f73b.js')
