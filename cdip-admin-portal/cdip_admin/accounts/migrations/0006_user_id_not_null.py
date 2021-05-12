9f45daa0d09df18df5a1f05bd25a34a7ac01cb93
Chris Doehring <chrisdoehring@gmail.com>
2021-05-12 09:05:13 -0700

    Fixing up migrations for accountprofile.user_id

--- cdip-admin-portal/cdip_admin/accounts/migrations/0006_user_id_not_null.py
+++ cdip-admin-portal/cdip_admin/accounts/migrations/0006_user_id_not_null.py
@@ -4,6 +4,20 @@ from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
 
+sentinel_user_username = 'sentinel-user'
+
+CREATE_SENTINEL_USER = f'''
+insert into auth_user(username, password, is_superuser, first_name, last_name, email, is_staff, is_active, date_joined) values (
+'{sentinel_user_username}', '8hgaeio4$im', false, 'Sentinel', 'Sentinel', 'sentinel-user@sintegrate.org', false, false, current_timestamp) on conflict do nothing;
+'''
+
+SET_NULLED_USER_IDS = f'''
+ update accounts_accountprofile ap set user_id = au.id from auth_user au where au.username='{sentinel_user_username}' and ap.user_id is null;
+'''
+
+ALTER_USER_ID_SET_NOT_NULL ='''
+alter table accounts_accountprofile alter column user_id set not null;
+'''
 
 class Migration(migrations.Migration):
 
@@ -13,10 +27,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='accountprofile',
-            name='user',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='auth.user'),
-            preserve_default=False,
-        ),
+        migrations.RunSQL('SET CONSTRAINTS ALL DEFERRED', reverse_sql='SET CONSTRAINTS ALL IMMEDIATE'),
+        migrations.RunSQL(CREATE_SENTINEL_USER, reverse_sql=migrations.RunSQL.noop),
+        migrations.RunSQL(SET_NULLED_USER_IDS, reverse_sql=migrations.RunSQL.noop),
+        migrations.RunSQL(ALTER_USER_ID_SET_NOT_NULL, reverse_sql=migrations.RunSQL.noop),
+        migrations.RunSQL('SET CONSTRAINTS ALL IMMEDIATE', reverse_sql='SET CONSTRAINTS ALL DEFERRED'),
     ]
