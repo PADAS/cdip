9f61f9d84808290058c3b407ee2560b2ebf6f5cb
Chris Doehring <chrisdoehring@gmail.com>
2021-06-07 15:22:33 -0700

    WIP on permissions in filter classes.

--- cdip-admin-portal/cdip_admin/accounts/admin.py
+++ cdip-admin-portal/cdip_admin/accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from accounts.models import AccountProfile
 
-# Register your models here.
+
+class OrganzationMemberInline(admin.TabularInline):
+    model = AccountProfile.organizations.through
+
+@admin.register(AccountProfile)
+class AccountProfile(admin.ModelAdmin):
+
+    list_display = ('username',)
+
+    inlines = [
+        OrganzationMemberInline,
+    ]
+
+    def username(self, obj):
+        return obj.user.username
+    username.short_description = 'Username'
+    search_fields = ('user__username', 'organizations__name',)
+
+    fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (('user',))
+        }
+        ),
+
+    )
