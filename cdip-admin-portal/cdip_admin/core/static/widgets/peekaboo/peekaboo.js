865a24c5615e68582fba78b7a9311b23d12e2c1d
Chris Doehring <chrisdoehring@gmail.com>
2021-06-10 21:37:46 -0700

    Update Ui for copying api key.

--- cdip-admin-portal/cdip_admin/core/static/widgets/peekaboo/peekaboo.js
+++ cdip-admin-portal/cdip_admin/core/static/widgets/peekaboo/peekaboo.js
@@ -1,28 +1,38 @@
-$(document).ready(function() {
-    $("#show_hide_password a").on('click', function(event) {
+$(document).ready(function () {
+    $("#show_hide_password a").on('click', function (event) {
         event.preventDefault();
         let sensative_text = event.target.parentElement.parentElement.querySelector('input')
         let peakaboo = event.target.parentElement.parentElement.querySelector('.peakaboo')
         let clip_board = event.target.parentElement.parentElement.querySelector('.copy-to-clip')
-        if($(sensative_text).attr("type") == "text"){
+        if ($(sensative_text).attr("type") == "text") {
             $(sensative_text).attr('type', 'password');
-            $(peakaboo).addClass( "fa-eye-slash" );
-            $(peakaboo).removeClass( "fa-eye" );
+            $(peakaboo).addClass("fa-eye-slash");
+            $(peakaboo).removeClass("fa-eye");
             $(clip_board).hide();
-        }else if($(sensative_text).attr("type") == "password"){
+        } else if ($(sensative_text).attr("type") == "password") {
             $(sensative_text).attr('type', 'text');
-            $(peakaboo).removeClass( "fa-eye-slash" );
-            $(peakaboo).addClass( "fa-eye" );
+            $(peakaboo).removeClass("fa-eye-slash");
+            $(peakaboo).addClass("fa-eye");
             $(clip_board).show();
         }
     });
 });
 
-function copyToClipboard(event) {
-    let r = document.createRange();
-    let element = event.target.parentElement.querySelector('input')
-    r.selectNode(element)
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(r);
+function copy_to_clipboard(event) {
+    let element = event.target.parentElement.querySelector('input');
+    element.select();
+    element.setSelectionRange(0, 99999);
     document.execCommand("copy");
+
+    let tooltip_element = event.target.querySelector('.tooltiptext');
+
+    tooltip_element.innerHTML = "Copied!";
 }
+
+
+function reset_tooltip(event) {
+    let tooltip_element = event.target.querySelector('.tooltiptext');
+    setTimeout()
+  tooltip_element.innerHTML = "Copy to clipboard";
+}
+
