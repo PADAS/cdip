284d752a1875f88853c5eee8ed124ed9017c45c6
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2022-02-02 17:41:43 -0500

    Adding in Update functionality for event types
    adding in command that allows us to kick off the sync logic
    
    adding in logic to update an event type if it detects difference in schema properties or definition

--- cdip_admin/sync_integrations/er_smart_sync.py
+++ cdip_admin/sync_integrations/er_smart_sync.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from urllib.parse import urlparse
 
 from dasclient.dasclient import DasClient
@@ -9,14 +10,16 @@ from integrations.models import InboundIntegrationConfiguration, OutboundIntegra
 from smartconnect import SmartClient, utils
 from smartconnect.er_sync_utils import build_earth_ranger_event_types, er_event_type_schemas_equal
 
+logger = logging.getLogger(__name__)
+
 CA_LABEL = 'Smart ER Integration Test CA [SMART_ER]'
 
 
 def push_smart_ca_data_model_to_er_event_types():
-    config = InboundIntegrationConfiguration.objects.get(type__slug='smart_connect', name__exact="SMART Connect 7.4.X")
-    smart_client = SmartClient(api=config.endpoint,
-                               username=config.login,
-                               password=config.password,
+    smart_config = OutboundIntegrationConfiguration.objects.get(type__slug='smart_connect', name__exact="SMART Connect 7.4.x")
+    smart_client = SmartClient(api=smart_config.endpoint,
+                               username=smart_config.login,
+                               password=smart_config.password,
                                use_language_code='en')
 
     # TODO: Get list of all outbounds associated to inbound
@@ -24,15 +27,15 @@ def push_smart_ca_data_model_to_er_event_types():
     # er_destinations = OutboundIntegrationConfiguration.objects.filter(devicegroup__devices__inbound_configuration=ibc).\
     #     annotate(inbound_type_slug=F('devicegroup__devices__inbound_configuration__type__slug')).distinct()
 
-    outbound_config = OutboundIntegrationConfiguration.objects.get(name__exact="ER SMART Test Site")
+    er_config = OutboundIntegrationConfiguration.objects.get(name__exact="ER SMART Test Site")
 
     provider_key = 'smart_connect'
-    url_parse = urlparse(outbound_config.endpoint)
+    url_parse = urlparse(er_config.endpoint)
 
-    return DasClient(service_root=outbound_config.endpoint,
-                     username=outbound_config.login,
-                     password=outbound_config.password,
-                     token=outbound_config.token,
+    das_client = DasClient(service_root=er_config.endpoint,
+                     username=er_config.login,
+                     password=er_config.password,
+                     token=er_config.token,
                      token_url=f"{url_parse.scheme}://{url_parse.hostname}/oauth2/token",
                      client_id="das_web_client",
                      provider_key=provider_key)
@@ -44,23 +47,16 @@ def push_smart_ca_data_model_to_er_event_types():
             ca_uuid = ca.uuid
             break
 
-    # print(f'CA UUID: {ca_uuid}')
-    # predicted_timezone = utils.guess_ca_timezone(ca)
-    # print(f'{ca.label} -- Predicted_timezone: {predicted_timezone}')
-    #
-    # present = datetime.now(tz=pytz.utc)
-    # print(f'Now is: {present.astimezone(predicted_timezone)} in {predicted_timezone}')
-
     dm = smart_client.download_datamodel(ca_uuid=ca_uuid)
     dm_dict = dm.export_as_dict()
 
     event_types = build_earth_ranger_event_types(dm_dict)
 
     existing_event_categories = das_client.get_event_categories()
-    event_category = next((x for x in existing_event_categories if x.get('value') == CA_LABEL[0:40]), None)
+    event_category = next((x for x in existing_event_categories if x.get('value') == CA_LABEL), None)
     if not event_category:
-        event_category = dict(value=CA_LABEL[0:40],
-                              display=CA_LABEL[0:40])
+        event_category = dict(value=CA_LABEL,
+                              display=CA_LABEL)
         das_client.post_event_category(event_category)
 
     existing_event_types = das_client.get_event_types()
@@ -69,25 +65,12 @@ def push_smart_ca_data_model_to_er_event_types():
             event_type_match = next((x for x in existing_event_types if x.get('value') == event_type.get('value')), None)
             if event_type_match:
                 event_type_match_schema = das_client.get_event_schema(event_type.get('value'))
-                if not er_event_type_schemas_equal(json.loads(event_type.get('schema')), event_type_match_schema):
-                    # TODO: Update Das Client to update event type
-                    pass
+                if not er_event_type_schemas_equal(json.loads(event_type.get('schema')).get('schema'),
+                                                   event_type_match_schema.get('schema')):
+                    event_type['id'] = event_type_match.get('id')
+                    das_client.patch_event_type(event_type)
             else:
                 event_type['category'] = event_category.get('value')
                 das_client.post_event_type(event_type)
     except Exception as e:
-        print(f'Exception raised posting event type {e}')
-        print(dict(event_type=event_type))
-
-    # smart_client.download_patrolmodel(ca_uuid=ca_uuid)
-    # smart_client.download_missionmodel(ca_uuid=ca_uuid)
-    #
-    # present = datetime.now(tz=pytz.utc)
-    #
-    # smart_client.add_incident(ca_uuid=ca_uuid)
-    # smart_client.add_patrol_trackpoint(ca_uuid=ca_uuid, device_id='dev-00009', x=-122.4526, y=48.4948, timestamp=present)
-    # smart_client.add_mission(ca_uuid=ca_uuid)
-
-
-if __name__ == '__main__':
-    push_smart_ca_data_model_to_er_event_types()
+        logger.exception(f'Exception raised posting event type', extra=dict(event_type=event_type))
