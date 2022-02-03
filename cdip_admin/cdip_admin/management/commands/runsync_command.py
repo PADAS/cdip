aedfa8dfd211a31e1f7b74491966468dd4a6f400
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2022-02-03 10:57:11 -0500

    Cleaning things up a bit
    Having command pass in arguments for integrations that it wants to sync
    
    Some refactoring of the sync process into a class

--- cdip_admin/cdip_admin/management/commands/runsync_command.py
+++ cdip_admin/cdip_admin/management/commands/runsync_command.py
@@ -1,15 +1,25 @@
 from django.core.management.base import BaseCommand
-from sync_integrations.er_smart_sync import push_smart_ca_data_model_to_er_event_types
+from sync_integrations.er_smart_sync import ERSMART_Synchronizer
 
 
-#  python manage.py runsync_command --settings=cdip_admin.local_settings
+#  runsync_command --settings=cdip_admin.local_settings --smart_integration_id=114499bc-8689-4e12-adc4-ac9be94eeae0 --er_integration_id=9e0d298a-01e0-4bfa-acc3-f0b446037095
 class Command(BaseCommand):
     help = 'A command to run a given synchronization process'
 
     def add_arguments(self, parser):
-        # TODO: Determine how we want to configure this command
-        pass
+        parser.add_argument('--smart_integration_id', type=str,
+                            help='SMART integration configuration id')
+
+        parser.add_argument('--er_integration_id', type=str,
+                            help='Earth Ranger integration configuration id')
 
     def handle(self, *args, **options):
-        # TODO: Determine how we want to handle the synchronization
-        push_smart_ca_data_model_to_er_event_types()
+        #TODO: In future can probably just pass in two integration ids and another param for type of sync
+        if options['smart_integration_id'] and options['er_integration_id']:
+            smart_integration_id = options['smart_integration_id']
+            er_integration_id = options['er_integration_id']
+            er_smart_sync = ERSMART_Synchronizer(smart_integration_id=smart_integration_id,
+                                                 er_integration_id=er_integration_id)
+            er_smart_sync.push_smart_ca_data_model_to_er_event_types()
+        else:
+            print("Arguments supplied are insufficient to run sync process")
