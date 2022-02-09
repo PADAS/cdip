13cb9be1db6efc6522932945b1ababecd4558108
Clemente Estrada <mirdrack@gmail.com>
2022-02-09 15:46:30 -0600

    Added local environment scaffolding

---
+++ cdip_admin/db/scripts/startup.sql
@@ -0,0 +1,7 @@
+-- This file is taken by the postgress container and is executed, it can be used
+-- to setup different settings on the database server
+
+-- Keycloak database creation
+CREATE DATABASE keycloak;
+-- Kong database creation
+CREATE DATABASE kong;
