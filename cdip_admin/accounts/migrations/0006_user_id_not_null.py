7d2280cd47f1d5504b5e8689717faf6172e4c5b4
Chris Doehring <chrisdoehring@gmail.com>
2022-02-22 12:57:46 -0800

    Add value for last_login for sentinel user.

--- cdip_admin/accounts/migrations/0006_user_id_not_null.py
+++ cdip_admin/accounts/migrations/0006_user_id_not_null.py
@@ -7,17 +7,18 @@ import django.db.models.deletion
 sentinel_user_username = 'sentinel-user'
 
 CREATE_SENTINEL_USER = f'''
-insert into auth_user(username, password, is_superuser, first_name, last_name, email, is_staff, is_active, date_joined) values (
-'{sentinel_user_username}', '8hgaeio4$im', false, 'Sentinel', 'Sentinel', 'sentinel-user@sintegrate.org', false, false, current_timestamp) on conflict do nothing;
+insert into auth_user(username, password, is_superuser, first_name, last_name, email, is_staff, is_active, date_joined, last_login) values (
+'{sentinel_user_username}', '8hgaeio4$im', false, 'Sentinel', 'Sentinel', 'sentinel-user@sintegrate.org', false, false, current_timestamp, current_timestamp) on conflict do nothing;
 '''
 
 SET_NULLED_USER_IDS = f'''
  update accounts_accountprofile ap set user_id = au.id from auth_user au where au.username='{sentinel_user_username}' and ap.user_id is null;
 '''
 
-ALTER_USER_ID_SET_NOT_NULL ='''
+ALTER_USER_ID_SET_NOT_NULL = '''
 alter table accounts_accountprofile alter column user_id set not null;
 '''
+
 
 class Migration(migrations.Migration):
 
@@ -27,9 +28,14 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL('SET CONSTRAINTS ALL DEFERRED', reverse_sql='SET CONSTRAINTS ALL IMMEDIATE'),
-        migrations.RunSQL(CREATE_SENTINEL_USER, reverse_sql=migrations.RunSQL.noop),
-        migrations.RunSQL(SET_NULLED_USER_IDS, reverse_sql=migrations.RunSQL.noop),
-        migrations.RunSQL(ALTER_USER_ID_SET_NOT_NULL, reverse_sql=migrations.RunSQL.noop),
-        migrations.RunSQL('SET CONSTRAINTS ALL IMMEDIATE', reverse_sql='SET CONSTRAINTS ALL DEFERRED'),
+        migrations.RunSQL('SET CONSTRAINTS ALL DEFERRED',
+                          reverse_sql='SET CONSTRAINTS ALL IMMEDIATE'),
+        migrations.RunSQL(CREATE_SENTINEL_USER,
+                          reverse_sql=migrations.RunSQL.noop),
+        migrations.RunSQL(SET_NULLED_USER_IDS,
+                          reverse_sql=migrations.RunSQL.noop),
+        migrations.RunSQL(ALTER_USER_ID_SET_NOT_NULL,
+                          reverse_sql=migrations.RunSQL.noop),
+        migrations.RunSQL('SET CONSTRAINTS ALL IMMEDIATE',
+                          reverse_sql='SET CONSTRAINTS ALL DEFERRED'),
     ]
