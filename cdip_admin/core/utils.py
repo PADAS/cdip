4b532d1e54fb332babe6c496f483dbdeb0ddfbff
Chris Doehring <chrisdoehring@gmail.com>
2022-03-11 17:17:24 -0800

    Set autocomplete="off" for fields with common names.

--- cdip_admin/core/utils.py
+++ cdip_admin/core/utils.py
@@ -7,20 +7,15 @@ from cdip_admin import settings
 
 logger = logging.getLogger(__name__)
 
-KEYCLOAK_SERVER = settings.KEYCLOAK_SERVER
-KEYCLOAK_REALM = settings.KEYCLOAK_REALM
-KEYCLOAK_ADMIN_CLIENT_ID = settings.KEYCLOAK_ADMIN_CLIENT_ID
-KEYCLOAK_ADMIN_CLIENT_SECRET = settings.KEYCLOAK_ADMIN_CLIENT_SECRET
-
-oauth_token_url = f"{KEYCLOAK_SERVER}/auth/realms/{KEYCLOAK_REALM}/protocol/openid-connect/token"
+oauth_token_url = f"{settings.KEYCLOAK_SERVER}/auth/realms/{settings.KEYCLOAK_REALM}/protocol/openid-connect/token"
 
 
 def get_admin_access_token():
     logger.debug('Getting Keycloak Admin Access Token')
     payload = {
         'grant_type': 'client_credentials',
-        'client_id': KEYCLOAK_ADMIN_CLIENT_ID,
-        'client_secret': KEYCLOAK_ADMIN_CLIENT_SECRET,
+        'client_id': settings.KEYCLOAK_ADMIN_CLIENT_ID,
+        'client_secret': settings.KEYCLOAK_ADMIN_CLIENT_SECRET,
     }
     response = requests.post(oauth_token_url,
                              data=payload)
