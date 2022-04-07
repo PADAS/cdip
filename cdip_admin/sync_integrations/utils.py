430922326a09d8749397b26e503249eece7a9fd6
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2022-04-07 16:51:35 -0600

    Remove reliance on ER OutboundConfig

--- cdip_admin/sync_integrations/utils.py
+++ cdip_admin/sync_integrations/utils.py
@@ -14,9 +14,9 @@ def run_er_smart_sync_integrations():
         if er_integration_id:
             # confirm er_integration exists
             try:
-                er_integration = OutboundIntegrationConfiguration.objects.get(id=er_integration_id)
-            except OutboundIntegrationConfiguration.DoesNotExist:
-                logger.error(f'er_integration_id specified was not found: {er_integration_id}')
+                er_integration = InboundIntegrationConfiguration.objects.get(id=er_integration_id)
+            except InboundIntegrationConfiguration.DoesNotExist:
+                logger.error(f'Earth Ranger Inbound Integration specified was not found: {er_integration_id}')
                 er_integration = None
             if not er_integration:
                 er_integration_id = None
@@ -26,11 +26,9 @@ def run_er_smart_sync_integrations():
             er_smart_sync.push_smart_ca_data_model_to_er_event_types()
             er_smart_sync.sync_patrol_datamodel()
             # TODO: create non-directional int so we dont have both inbound and outbound int representing same system
-            er_inbound_integration = InboundIntegrationConfiguration.objects.get(endpoint=er_integration.endpoint,
-                                                                                 enabled=True)
-            if er_inbound_integration:
-                er_smart_sync.get_er_events(config=er_inbound_integration)
-                er_smart_sync.get_er_patrols(config=er_inbound_integration)
+            if er_integration:
+                er_smart_sync.get_er_events(config=er_integration)
+                er_smart_sync.get_er_patrols(config=er_integration)
 
         else:
             logger.warning(f"skipping sync, er_integration_id not found for smart_integration {smart_integration.id}")
