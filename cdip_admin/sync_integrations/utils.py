ca701a303ee5f75a37f47380bb4d5ff883ecf699
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2022-04-14 15:47:05 -0400

    Logging

--- cdip_admin/sync_integrations/utils.py
+++ cdip_admin/sync_integrations/utils.py
@@ -21,21 +21,28 @@ def run_er_smart_sync_integrations():
             if not er_integration:
                 er_integration_id = None
         if smart_integration_id and er_integration_id:
+            logger.info(f"Beginning SMART ER sync process", extra=dict(smart_integration_id=smart_integration_id,
+                                                                       smart_integration_name=smart_integration.name,
+                                                                       er_integration_id=er_integration_id,
+                                                                       er_integration_name=er_integration.name))
             er_smart_sync = ER_SMART_Synchronizer(smart_integration_id=smart_integration_id,
                                                   er_integration_id=er_integration_id)
             caslist = er_smart_sync.smart_client.get_conservation_areas()
 
             # Handle Group of CA's associated to single ER site
             for smart_ca_uuid in er_smart_sync.smart_ca_uuids:
+                logger.debug(f'Processing SMART CA: {smart_ca_uuid}')
                 ca_match = next((ca for ca in caslist if str(ca.uuid) == smart_ca_uuid), None)
                 if not ca_match:
                     logger.warning(f'Conservation Area not found', extra=dict(smart_ca_uuid=smart_ca_uuid))
                     return
+                logger.debug(f'Beginning sync of event types: {smart_ca_uuid}')
                 er_smart_sync.push_smart_ca_data_model_to_er_event_types(smart_ca_uuid=smart_ca_uuid, ca=ca_match)
                 er_smart_sync.sync_patrol_datamodel(smart_ca_uuid=smart_ca_uuid)
 
             # TODO: create non-directional int so we dont have both inbound and outbound int representing same system
             if er_integration:
+                logger.debug(f'Beginning pull of ER objects')
                 er_smart_sync.get_er_events(config=er_integration)
                 er_smart_sync.get_er_patrols(config=er_integration)
 
