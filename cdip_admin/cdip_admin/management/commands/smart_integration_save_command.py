740d46609f0f64720df62db3d1d24be4a25b1b22
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2022-06-28 10:15:23 -0400

    Check in work in progress

---
+++ cdip_admin/cdip_admin/management/commands/smart_integration_save_command.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+import logging
+
+from sync_integrations.utils import on_smart_integration_save
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "A command to run appropriate actions after a SMART integration configuration is saved"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--smart_integration_id",
+            type=str,
+            help="SMART integration configuration id",
+        )
+
+    def handle(self, *args, **options):
+        try:
+            smart_integration_id = options.get("smart_integration_id")
+            if smart_integration_id:
+                on_smart_integration_save(integration_id=smart_integration_id)
+        except:
+            # TODO: raise a PortalBackgroundProcessError
+            pass
