80575225ad2e9af0fd28cff85e8dc31e7cc8d429
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2022-06-28 10:15:23 -0400

    Check in work in progress

--- cdip_admin/sync_integrations/utils.py
+++ cdip_admin/sync_integrations/utils.py
@@ -1,9 +1,18 @@
+import uuid
+
+from smartconnect import SmartClient
+from smartconnect.models import (
+    ConservationArea,
+)
+
 from integrations.models import (
     OutboundIntegrationConfiguration,
     InboundIntegrationConfiguration,
 )
+from sync_integrations.cache import cache
 from sync_integrations.er_smart_sync import ER_SMART_Synchronizer
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -91,5 +100,63 @@ def run_er_smart_sync_integrations():
             continue
 
 
-if __name__ == "__main__":
-    run_er_smart_sync_integrations()
+def on_smart_integration_save(*, integration_id: str):
+    config = OutboundIntegrationConfiguration.objects.get(id=integration_id)
+    smart_client = SmartClient(
+        api=config.endpoint,
+        username=config.login,
+        password=config.password,
+        use_language_code="en",
+    )
+    ca_uuids = config.additional.get('ca_uuids')
+    for ca_uuid in ca_uuids:
+        ca = get_conservation_area(ca_uuid=ca_uuid)
+
+
+def get_conservation_area(self, *, ca_uuid: str = None):
+    cache_key = f"cache:smart-ca:{ca_uuid}:metadata"
+    self.logger.info(f"Looking up CA cached at {cache_key}.")
+    try:
+        cached_data = cache.cache.get(cache_key)
+        if cached_data:
+            self.logger.info(f"Found CA cached at {cache_key}.")
+            self.ca = ConservationArea.parse_raw(cached_data)
+            return self.ca
+
+        self.logger.info(f"Cache miss for {cache_key}")
+    except:
+        self.logger.info(f"Cache miss/error for {cache_key}")
+        pass
+
+    try:
+        self.logger.info(
+            "Querying Smart Connect for CAs at endpoint: %s, username: %s",
+            self._config.endpoint,
+            self._config.login,
+        )
+
+        for ca in self.smartconnect_client.get_conservation_areas():
+            if ca.uuid == uuid.UUID(ca_uuid):
+                self.ca = ca
+                break
+        else:
+            logger.error(
+                f"Can't find a Conservation Area with UUID: {self.ca_uuid}"
+            )
+            self.ca = None
+
+        if ca:
+            self.logger.info(f"Caching CA metadata at {cache_key}")
+            cache.cache.setex(
+                name=cache_key,
+                time=60 * 5,
+                value=json.dumps(dict(self.ca), default=str),
+            )
+
+        return ca
+
+    except Exception as ex:
+        self.logger.exception(
+            f"Failed to get Conservation Areas", extra=dict(ca_uuid=ca_uuid)
+        )
+        # raise ReferenceDataError(f"Failed to get SMART Conservation Areas")
