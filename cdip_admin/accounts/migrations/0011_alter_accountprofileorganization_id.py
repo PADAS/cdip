860694733f40d2dceacdad266174d7cab274d143
Chris Doehring <chrisdoehring@gmail.com>
2022-08-31 11:41:09 -0700

    Black formatted.

--- cdip_admin/accounts/migrations/0011_alter_accountprofileorganization_id.py
+++ cdip_admin/accounts/migrations/0011_alter_accountprofileorganization_id.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0010_auto_20210729_2134'),
+        ("accounts", "0010_auto_20210729_2134"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='accountprofileorganization',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="accountprofileorganization",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
     ]
--- cdip_admin/core/migrations/0006_alter_task_id.py
+++ cdip_admin/core/migrations/0006_alter_task_id.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0005_auto_20211124_1454'),
+        ("core", "0005_auto_20211124_1454"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='task',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="task",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
     ]
