3c34495f97c0ed0131862ae301f9515b04142976
Mariano M <marianom@earthranger.com>
2023-03-16 09:05:36 -0300

    Move invite and remove actions to members viewset

--- cdip_admin/api/v2/views.py
+++ cdip_admin/api/v2/views.py
@@ -16,10 +16,6 @@ class OrganizationView(viewsets.ModelViewSet):
     """
 
     def get_serializer_class(self):
-        if self.action == "invite":
-            return v2_serializers.InviteUserSerializer
-        if self.action == "remove_members":
-            return v2_serializers.RemoveMemberSerializer
         return v2_serializers.OrganizationSerializer
 
     def get_queryset(self):
@@ -37,46 +33,14 @@ class OrganizationView(viewsets.ModelViewSet):
             return Organization.objects.none()
         return user.accountprofile.organizations.all()
 
-    @action(detail=True, methods=['post', 'put'])
-    def invite(self, request, pk=None):
-        """
-        Invite members to an Organization
-        """
-        # ToDo: Check permissions
-        # Only superusers or org admins can invite members
-        # Validations
-        serializer = self.get_serializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        # Create user
-        self.perform_create(serializer)
-        # ToDo: Analyze new cases when we want to send emails
-        # Consider using some third-party email service
-        return Response({'status': 'User invited successfully'})
 
-    @action(detail=True, methods=['post', 'put', 'patch'], url_path="remove-members")
-    def remove_members(self, request, pk=None):
-        requester = self.request.user
-        # ToDo: validate Permissions?
-        org = self.get_object()
-        serializer = self.get_serializer(
-            data=request.data
-        )
-        serializer.is_valid(raise_exception=True)
-        # Remove members from organization
-        removed_qty = remove_members_from_organization(org_id=org.id, profile_ids=serializer.validated_data["member_ids"])
-        return Response(data={"removed": removed_qty}, status=status.HTTP_200_OK)
-
-
-class MemberViewSet(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.ListModelMixin,
-                    GenericViewSet):
+class MemberViewSet(viewsets.ModelViewSet):
     # ToDo: validate Permissions?
     def get_serializer_class(self):
+        if self.action == "invite":
+            return v2_serializers.InviteUserSerializer
+        if self.action == "remove_members":
+            return v2_serializers.RemoveMemberSerializer
         if self.action == "update":
             return v2_serializers.OrganizationMemberUpdateSerializer
         return v2_serializers.OrganizationMemberRetrieveSerializer
@@ -97,3 +61,35 @@ class MemberViewSet(mixins.RetrieveModelMixin,
             instance._prefetched_objects_cache = {}
         # Return the data which was updated
         return Response(serializer.validated_data)
+
+    @action(detail=False, methods=['post', 'put'])
+    def invite(self, request, organization_pk=None):
+        """
+        Invite members to an Organization
+        """
+        # ToDo: Check permissions
+        # Only superusers or org admins can invite members
+        # Validations
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        # Create user
+        self.perform_create(serializer)
+        # ToDo: Analyze new cases when we want to send emails
+        # Consider using some third-party email service
+        return Response({'status': 'User invited successfully'})
+
+    @action(detail=False, methods=['post', 'put', 'patch'], url_path="remove-members")
+    def remove(self, request, organization_pk=None):
+        requester = self.request.user
+        # ToDo: validate Permissions?
+        serializer = self.get_serializer(
+            data=request.data
+        )
+        serializer.is_valid(raise_exception=True)
+        # Remove members from organization
+        removed_qty = remove_members_from_organization(org_id=organization_pk, profile_ids=serializer.validated_data["member_ids"])
+        return Response(data={"removed": removed_qty}, status=status.HTTP_200_OK)
