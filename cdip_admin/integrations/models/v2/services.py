54938200f2cff797cf87a9540a172331e605b803
Mariano M <marianom@earthranger.com>
2023-05-09 18:06:39 -0300

    Add filters in the integration types endpoint

--- cdip_admin/integrations/models/v2/services.py
+++ cdip_admin/integrations/models/v2/services.py
@@ -1,4 +1,7 @@
-from .models import RoutingRule
+from django.db.models import Subquery
+
+from accounts.utils import get_user_organizations_qs
+from .models import Integration, IntegrationType, RoutingRule
 
 
 def ensure_default_routing_rule(integration):
@@ -8,10 +11,18 @@ def ensure_default_routing_rule(integration):
         routing_rule, _ = RoutingRule.objects.get_or_create(
             owner_id=integration.owner.id,
             name=name,
-            #type=default_routing_type
         )
         integration.default_routing_rule = routing_rule
         integration.save()
     # Add the integration a provider in its default routing rule
     if not integration.default_routing_rule.data_providers.filter(id=integration.id).exists():
         integration.default_routing_rule.data_providers.add(integration)
+
+
+def get_user_integrations_qs(user):
+    # Return a list with the integrations that the currently authenticated user is allowed to see.
+    user_organizations = get_user_organizations_qs(user=user)
+    integrations = Integration.objects.filter(
+        owner__in=Subquery(user_organizations.values('id'))
+    )
+    return integrations
