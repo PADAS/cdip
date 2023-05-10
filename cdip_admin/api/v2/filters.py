6a80b672fc439e1da29424d31c9c3dcab6b23418
Mariano M <marianom@earthranger.com>
2023-05-10 14:41:15 -0300

    Add a search filter for integration types

--- cdip_admin/api/v2/filters.py
+++ cdip_admin/api/v2/filters.py
@@ -1,1 +1,12 @@
-import django_filters
+from rest_framework import filters
+
+
+class CustomizableSearchFilter(filters.SearchFilter):
+    """
+    This search filter allows specifying the search_fields dynamically with a query parameter
+    """
+    def get_search_fields(self, view, request):
+        if search_fields := request.query_params.get('search_fields'):
+            return search_fields.split(",")
+        # If the query param is not set, check in the view
+        return super().get_search_fields(view, request)
