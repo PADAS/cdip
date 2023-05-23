0942806e28bd5ed0b22edcae4aa2af8a955dde14
Mariano M <marianom@earthranger.com>
2023-05-23 17:09:01 -0300

    Update celery beat settings for er2smart sync task

--- cdip_admin/cdip_admin/celery.py
+++ cdip_admin/cdip_admin/celery.py
@@ -52,7 +52,7 @@ app.conf.beat_schedule = {
     },
     # Run sync integrations
     "run-sync-integrations": {
-        "task": "cdip_admin.tasks.run_sync_integrations",
+        "task": "sync_integrations.tasks.run_sync_integrations",
         "schedule": timedelta(
             minutes=settings.CELERY_TASK_SYNC_INTEGRATION_INTERVAL_MINUTES
         ),
