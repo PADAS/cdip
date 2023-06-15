9b6e033212d9de94c8c3de9cbfb500e1ea510e58
Mariano M <marianom@earthranger.com>
2023-06-15 14:43:52 -0300

    Update instrumentation code for events

--- cdip_admin/core/tracing/instrumentation.py
+++ cdip_admin/core/tracing/instrumentation.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from cdip_connector.core.schemas.v2 import Event
+from gundi_core.schemas.v2 import Event
 
 
 def _enrich_span_from_kwargs(span, **kwargs):
@@ -16,22 +16,24 @@ def enrich_span_from_event(span, event: Event, **kwargs):
     This helper function adds attributes to a span extracting relevant data from an event.
     It also supports passing extra key/value pairs as attributes.
     """
+    span.set_attribute("gundi_id", str(event.gundi_id))
     span.set_attribute("data", str(event.dict()))
-    span.set_attribute("integration_id", str(event.integration_id))
+    span.set_attribute("integration_id", str(event.data_provider_id))
     span.set_attribute("observation_type", event.observation_type)
     span.set_attribute("source_id", str(event.source_id))
     span.set_attribute("event_type", str(event.event_type))
     span.set_attribute("event_location", str(event.location.dict()))
     span.set_attribute("event_title", event.title)
     span.set_attribute("event_geometry", str(event.geometry))
-    # elif isinstance(event, EREvent):
-    #     span.set_attribute("event_owner", event.owner)
-    #     span.set_attribute("event_state", str(event.state))
-    #     span.set_attribute("event_priority", str(event.priority))
+    # ToDo: Check what was this for:
+    # span.set_attribute("event_owner", event.owner)
+    # span.set_attribute("event_state", str(event.state))
+    # span.set_attribute("event_priority", str(event.priority))
     _enrich_span_from_kwargs(span, **kwargs)
 
 
 def enrich_span_from_attachment(span, attachment, **kwargs):
-    span.set_attribute("integration_id", str(attachment.integration_id))
+    span.set_attribute("gundi_id", str(attachment.gundi_id))
+    span.set_attribute("integration_id", str(attachment.data_provider_id))
     span.set_attribute("observation_type", str(attachment.observation_type))
     _enrich_span_from_kwargs(span, **kwargs)
