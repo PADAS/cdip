4b0c7a3d1fc6f2402d23ecded5c7e417a040a1ea
Mariano M <marianom@earthranger.com>
2023-06-22 10:26:29 -0300

    Fixes in instrumentation for events v2

--- cdip_admin/core/tracing/instrumentation.py
+++ cdip_admin/core/tracing/instrumentation.py
@@ -16,7 +16,7 @@ def enrich_span_from_event(span, event: Event, **kwargs):
     This helper function adds attributes to a span extracting relevant data from an event.
     It also supports passing extra key/value pairs as attributes.
     """
-    span.set_attribute("gundi_id", str(event.object_id))
+    span.set_attribute("gundi_id", str(event.gundi_id))
     span.set_attribute("data", str(event.dict()))
     span.set_attribute("integration_id", str(event.data_provider_id))
     span.set_attribute("observation_type", event.observation_type)
@@ -25,15 +25,11 @@ def enrich_span_from_event(span, event: Event, **kwargs):
     span.set_attribute("event_location", str(event.location.dict()))
     span.set_attribute("event_title", event.title)
     span.set_attribute("event_geometry", str(event.geometry))
-    # ToDo: Check what was this for:
-    # span.set_attribute("event_owner", event.owner)
-    # span.set_attribute("event_state", str(event.state))
-    # span.set_attribute("event_priority", str(event.priority))
     _enrich_span_from_kwargs(span, **kwargs)
 
 
 def enrich_span_from_attachment(span, attachment, **kwargs):
-    span.set_attribute("gundi_id", str(attachment.object_id))
+    span.set_attribute("gundi_id", str(attachment.gundi_id))
     span.set_attribute("integration_id", str(attachment.data_provider_id))
     span.set_attribute("observation_type", str(attachment.observation_type))
     _enrich_span_from_kwargs(span, **kwargs)
