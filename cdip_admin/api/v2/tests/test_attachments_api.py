0630596c3941e31d180e7bf6404f0580b9b4d647
Mariano M <marianom@earthranger.com>
2023-07-06 12:33:19 -0300

    Test validations in the attachments endpoint

--- cdip_admin/api/v2/tests/test_attachments_api.py
+++ cdip_admin/api/v2/tests/test_attachments_api.py
@@ -36,7 +36,7 @@ def test_create_single_attachment(
     _test_create_attachment(
         api_client=api_client,
         keyauth_headers=keyauth_headers_trap_tagger,
-        event_id=str(trap_tagger_event_trace.id),
+        event_id=str(trap_tagger_event_trace.object_id),
         files={
             "file1": leopard_image_file
         }
@@ -63,7 +63,7 @@ def test_create_attachments_multiple_files(
     _test_create_attachment(
         api_client=api_client,
         keyauth_headers=keyauth_headers_trap_tagger,
-        event_id=str(trap_tagger_event_trace.id),
+        event_id=str(trap_tagger_event_trace.object_id),
         files={
             "file1": leopard_image_file,
             "file2": wilddog_image_file
@@ -80,3 +80,17 @@ def test_create_attachments_multiple_files(
         data=ANY,
         extra=ANY
     )
+
+
+def test_create_attachments_no_files(
+        api_client, mocker, mock_get_publisher, mock_deduplication, keyauth_headers_trap_tagger,
+        trap_tagger_event_trace
+):
+    # Try sending no files
+    response = api_client.post(
+        reverse("attachments-list", kwargs={"event_pk": trap_tagger_event_trace.object_id}),
+        format='multipart',
+        **keyauth_headers_trap_tagger
+    )
+    # Expect a 400 response
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
