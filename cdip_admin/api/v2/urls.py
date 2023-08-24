34e1ac05791b40d43f86cf9ff50791f0a1b8a535
Mariano M <marianom@earthranger.com>
2023-08-24 19:23:11 -0300

    Add the observations endpoint in API v2

--- cdip_admin/api/v2/urls.py
+++ cdip_admin/api/v2/urls.py
@@ -15,6 +15,7 @@ default_router.register('integrations', views.IntegrationsView, basename="integr
 default_router.register('connections', views.ConnectionsView, basename="connections")
 default_router.register('sources', views.SourcesView, basename="sources")
 default_router.register('routes', views.RoutesView, basename="routes")
+default_router.register('observations', views.ObservationsView, basename="observations")
 default_router.register('events', views.EventsView, basename="events")
 events_router = NestedSimpleRouter(default_router, r'events', lookup='event')
 events_router.register(r'attachments', views.AttachmentViewSet, basename='attachments')
