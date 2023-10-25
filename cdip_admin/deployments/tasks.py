e0c10ea82bbdc5645b3ead48cc77f3e89549b18a
Mariano Martinez Grasso <marianom@earthranger.com>
2023-10-25 10:34:56 -0300

    Update dispatcher deployment tasks to fallback to default settings

--- cdip_admin/deployments/tasks.py
+++ cdip_admin/deployments/tasks.py
@@ -10,10 +10,11 @@ from . import utils
 if settings.GCP_ENVIRONMENT_ENABLED:
     pubsub_client = pubsub_v1.PublisherClient()  # This raises  credentials error when running in th CI pipeline
     functions_client = functions_v2.FunctionServiceClient()
+    DISPATCHER_DEFAULT_SETTINGS = utils.get_dispatcher_defaults_from_gcp_secrets()
 else:
     pubsub_client = utils.PubSubDummyClient()
     functions_client = utils.FunctionsDummyClient()
-
+    DISPATCHER_DEFAULT_SETTINGS = {}
 
 
 @shared_task
@@ -21,25 +22,30 @@ def deploy_serverless_dispatcher(deployment_id):
     DispatcherDeployment = apps.get_model("deployments", "DispatcherDeployment")
     deployment = DispatcherDeployment.objects.get(id=deployment_id)
     deployment.status = DispatcherDeployment.Status.IN_PROGRESS
+    deployment.status_details = ""  # Clean previous errors
     deployment.save()
 
     # Get settings from the database
     if deployment.integration:  # v2 models
+        gundi_version = "v2"
         integration = deployment.integration
     elif deployment.legacy_integration:  # legacy models
+        gundi_version = "v1"
         integration = deployment.legacy_integration
     else:
         error_msg = f"Either integration or legacy_integration field must be set"
         print(error_msg)
         deployment.status = DispatcherDeployment.Status.ERROR
-        deployment.status_details = error_msg
+        deployment.status_details = error_msg[:500]
         deployment.save()
         return
-    function_name = deployment.name or f"dispatch-{integration.id}-dev"
+    function_name = deployment.name or utils.get_default_dispatcher_name(integration=integration, gundi_version=gundi_version)
+    if not deployment.configuration:  # Use default settings
+        deployment.configuration = DISPATCHER_DEFAULT_SETTINGS
     configuration = deployment.configuration
     env_vars = configuration.get("env_vars", {})
     project_id = env_vars.get("GCP_PROJECT_ID")
-    topic = integration.additional.get("topic", "")  # ToDo: Use a default name
+    topic = integration.additional.get("topic", utils.get_default_topic_name(integration=integration, gundi_version=gundi_version))  # ToDo: Use a default name
     topic_path = f'projects/{project_id}/topics/{topic}'
     deployment.topic_name = topic  # Save the topic for retries or deletions
     deployment.save()
@@ -56,13 +62,12 @@ def deploy_serverless_dispatcher(deployment_id):
         print(f"Deploy complete.")
         print(response)
         deployment.status = DispatcherDeployment.Status.COMPLETE
-        deployment.status_details = ""  # Clean previous errors
         deployment.save()
     except Exception as e:  # ToDo: Catch more specific errors like validation errors?
         error_msg = f"Error deploying function: {e}"
         print(error_msg)
         deployment.status = DispatcherDeployment.Status.ERROR
-        deployment.status_details = error_msg
+        deployment.status_details = error_msg[:500]
         deployment.save()
         return
 
@@ -74,6 +79,7 @@ def delete_serverless_dispatcher(deployment_id, topic):
     deployment.integration = None  # Unlink from integrations as they might be being deleted
     deployment.legacy_integration = None
     deployment.status = DispatcherDeployment.Status.DELETING
+    deployment.status_details = ""  # Clean previous errors
     deployment.save()
 
     function_name = deployment.name
@@ -88,7 +94,7 @@ def delete_serverless_dispatcher(deployment_id, topic):
         error_msg = f"Error deleting function: {e}"
         print(error_msg)
         deployment.status = DispatcherDeployment.Status.ERROR
-        deployment.status_details = error_msg
+        deployment.status_details = error_msg[:500]
         deployment.save()
     else:  # No errors deleting resources in GCP
         deployment.delete()  # Remove it from the DB
@@ -198,3 +204,4 @@ def delete_topic(topic_name):
         topic=topic_name,
     )
     pubsub_client.delete_topic(request=request)
+
