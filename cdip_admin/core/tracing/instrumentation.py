6b5565e9214a52fdf2bba8f17b67b40398d7e3c3
Mariano Martinez Grasso <marianom@earthranger.com>
2024-04-11 19:00:33 -0300

    Improve tracing for EREvents in ER-SMART sync

--- cdip_admin/core/tracing/instrumentation.py
+++ cdip_admin/core/tracing/instrumentation.py
@@ -1,6 +1,7 @@
 import logging
 from django.conf import settings
 from gundi_core.schemas.v2 import Event, Observation
+from gundi_core.schemas.v1 import EREvent
 from opentelemetry import propagate
 
 
@@ -31,6 +32,26 @@ def enrich_span_from_event(span, event: Event, **kwargs):
     span.set_attribute("event_type", str(event.event_type))
     span.set_attribute("event_title", event.title)
     span.set_attribute("event_geometry", str(event.geometry))
+    _enrich_span_from_kwargs(span, **kwargs)
+
+
+def enrich_span_from_er_event(span, event: EREvent, **kwargs):
+    """
+    This helper function adds attributes to a span extracting relevant data from an Earth Ranger Event.
+    It also supports passing extra key/value pairs as attributes.
+    """
+    span.set_attribute("er_uuid", str(event.er_uuid))
+    span.set_attribute("serial_number", str(event.serial_number))
+    span.set_attribute("integration_id", str(event.integration_id))
+    span.set_attribute("observation_type", event.observation_type)
+    span.set_attribute("device_id", str(event.device_id))
+    span.set_attribute("location", str(event.location.dict()) if event.location else "no-location")
+    span.set_attribute("data", str(event.dict()))
+    span.set_attribute("event_type", str(event.event_type))
+    span.set_attribute("event_title", event.title)
+    span.set_attribute("event_details", event.event_details)
+    span.set_attribute("patrols", str(event.patrols))
+    span.set_attribute("patrols", str(event.files))
     _enrich_span_from_kwargs(span, **kwargs)
 
 
