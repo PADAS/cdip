610ff6d78346825d8bca3dcec910d45d26be20d4
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2024-07-01 19:30:39 -0300

    Basic test coverage for config overrides

--- cdip_admin/api/v2/tests/test_actions_execution_api.py
+++ cdip_admin/api/v2/tests/test_actions_execution_api.py
@@ -17,11 +17,11 @@ def _test_execute_action(
     mocker.patch("integrations.models.v2.models.google.oauth2.id_token.fetch_id_token", mocker.MagicMock(return_value="fake_id_token"))
     integration_service_url = integration.type.service_url
     actions_execute_url = urljoin(integration_service_url, "/v1/actions/execute")
-    requests_mock.post(actions_execute_url, json=action_response, status_code=status.HTTP_200_OK)
+    gcp_request_mock = requests_mock.post(actions_execute_url, json=action_response, status_code=status.HTTP_200_OK)
     api_url = reverse("actions-execute", kwargs={"integration_pk": integration.id, "value": action.value})
     request_data = {}
     if config_overrides:
-        request_data["configurations"] = config_overrides
+        request_data["config_overrides"] = config_overrides
     if run_in_background:
         request_data["run_in_background"] = True
     api_client.force_authenticate(user)
@@ -34,6 +34,8 @@ def _test_execute_action(
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == expected_response
+    if config_overrides:
+        assert gcp_request_mock.last_request.json().get("config_overrides") == config_overrides
 
 
 def _test_cannot_execute_action(
@@ -106,4 +108,21 @@ def test_cannot_execute_action_as_org_viewer(
         user=org_viewer_user,
         integration=cellstop_integration,
         action=cellstop_action_auth
+    )
+
+
+def test_execute_action_with_config_overrides(
+        api_client, mocker, requests_mock, org_admin_user, organization,
+        cellstop_integration, cellstop_action_auth, cellstop_action_auth_response
+):
+    _test_execute_action(
+        mocker=mocker,
+        api_client=api_client,
+        requests_mock=requests_mock,
+        user=org_admin_user,
+        integration=cellstop_integration,
+        action=cellstop_action_auth,
+        action_response=cellstop_action_auth_response,
+        expected_response=cellstop_action_auth_response,
+        config_overrides={"username": "test_user", "password": "test_password"}  # pragma: allowlist secret
     )
