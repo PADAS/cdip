14808a23bd891df65a17dad64d00dcef5c8a5d4a
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2024-11-07 09:25:35 -0300

    Test coverage for source id in activity logs

--- cdip_admin/api/v2/tests/utils.py
+++ cdip_admin/api/v2/tests/utils.py
@@ -3,22 +3,25 @@ from activity_log.models import ActivityLog
 
 
 def _test_activity_logs_on_instance_created(activity_log, instance, user):
-    model = str(instance._meta.model.__name__)
+    Model = instance._meta.model
+    model_name = str(Model.__name__)
     details = activity_log.details
     assert activity_log
     assert activity_log.log_type == ActivityLog.LogTypes.DATA_CHANGE
     assert activity_log.log_level == ActivityLog.LogLevels.INFO
     assert activity_log.origin == ActivityLog.Origin.PORTAL
-    expected_title = f"{model} {ActivityActions.CREATED.value}"
+    expected_title = f"{model_name} {ActivityActions.CREATED.value}"
+    if hasattr(Model, "logs_id_field"):
+        expected_title += f" ({getattr(instance, Model.logs_id_field)})"
     if user and not user.is_anonymous:
         expected_title += f" by {user}"
     assert activity_log.title == expected_title
-    assert details.get("model_name") == model
+    assert details.get("model_name") == model_name
     assert details.get("instance_pk") == str(instance.pk)
     assert details.get("action") == ActivityActions.CREATED.value
     assert activity_log.is_reversible
     assert activity_log.revert_data == {
-        "model_name": model,
+        "model_name": model_name,
         "instance_pk": str(instance.pk)
     }
 
