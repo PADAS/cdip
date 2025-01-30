43adf00c300d1d5e02530a566dc20be514a91389
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2025-01-30 15:08:59 -0300

    Save extra details in activity logs o connector errors

--- cdip_admin/event_consumers/integration_events_consumer.py
+++ cdip_admin/event_consumers/integration_events_consumer.py
@@ -66,9 +66,11 @@ def handle_integration_action_failed_event(event_dict: dict):
     action_id = event_data.action_id
     integration_id = event_data.integration_id
     integration = Integration.objects.get(id=integration_id)
-    error = event_dict["payload"].get("error", "No details.")
+    error = event_data.payload.error
     message = f"Error running action '{action_id}': {error}"
     logger.info(message, extra={"event": event_dict})
+    # Workaround to serialize complex types until upgrading to pydantic v2
+    log_data_clean = json.loads(json.dumps(event_data.dict(), default=str))
     ActivityLog.objects.create(
         log_level=ActivityLog.LogLevels.ERROR,
         log_type=ActivityLog.LogTypes.EVENT,
@@ -76,7 +78,7 @@ def handle_integration_action_failed_event(event_dict: dict):
         integration=integration,
         value="integration_action_failed",
         title=_clean_event_title(message),
-        details=event_dict.get("payload", {}),
+        details=log_data_clean,
         is_reversible=False,
     )
 
