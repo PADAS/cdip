1d6f6c6b6d0c507532afd77828165db00e74ad47
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2025-02-07 16:47:34 -0300

    Extra test coverage for data frequency metric

--- cdip_admin/integrations/tests/test_metrics.py
+++ cdip_admin/integrations/tests/test_metrics.py
@@ -9,9 +9,23 @@ pytestmark = pytest.mark.django_db
 @pytest.mark.parametrize(
     "observation_traces_spikes,expected_metrics",
     [
-        ([(24, 'hours'), (16, 'hours'), (8, 'hours'), (1, 'hours'), (0, 'hours'),], [60, 480, 480]),  # Every 8 hours
-        ([(300, 'minutes'), (180, 'minutes'), (150, 'minutes'), (120, 'minutes'), (112, 'minutes'), (90, 'minutes'), (60, 'minutes'), ], [10, 120, 30]),  # Every 30 minutes
-
+        (  # Mostly every 8 hours with one extra spike in the middle
+                [(24, 'hours'), (16, 'hours'), (8, 'hours'), (1, 'hours'), (0, 'hours'), ],
+                [60, 480, 480]
+        ),
+        (  # Most commonly every 30 minutes with some outliers
+                [(300, 'minutes'), (180, 'minutes'), (150, 'minutes'), (120, 'minutes'), (112, 'minutes'),
+                 (90, 'minutes'), (60, 'minutes'), ],
+                [10, 120, 30]
+        ),
+        (  # Exactly every 10 minutes
+                [(50, 'minutes'), (40, 'minutes'), (30, 'minutes'), (20, 'minutes'), (10, 'minutes'),],
+                [10, 10, 10]
+        ),
+        (  # Every about 10 minutes
+                [(50, 'minutes'), (40, 'minutes'), (29, 'minutes'), (20, 'minutes'), (7, 'minutes'),],
+                [10, 10, 10]  # Intervals are rounded to nearest 10 minutes
+        ),
     ],
     indirect=['observation_traces_spikes']
 )
