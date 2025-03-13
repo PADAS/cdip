43683c5f1745497a22547fbfdf118ef1107d9191
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2025-03-13 14:56:05 -0300

    Test coverage for periodic tasks with long names

--- cdip_admin/integrations/tests/test_actions_scheduling.py
+++ cdip_admin/integrations/tests/test_actions_scheduling.py
@@ -1,6 +1,8 @@
 import pytest
 import json
 from django_celery_beat.models import  PeriodicTask
+
+from integrations.models import Integration
 from ..models import IntegrationConfiguration
 
 
@@ -48,3 +50,35 @@ def test_enable_periodic_task_on_integration_enabled(provider_lotek_panthera, lo
     provider_lotek_panthera.save()
     periodic_task = PeriodicTask.objects.get(task="integrations.tasks.run_integration")
     assert periodic_task.enabled
+
+
+def test_periodic_task_is_created_for_action_with_long_name(
+        organization,
+        integration_type_lotek,
+        lotek_action_auth,
+        lotek_action_pull_positions,
+):
+    integration_name = f"Lotek Integration with l{''.join(['o']*169)}ng name"  # 200 characters in total
+    provider_integration = Integration.objects.create(
+        type=integration_type_lotek,
+        name=integration_name,
+        owner=organization,
+        base_url=f"api.test.lotek.com",
+    )
+    # Configure actions
+    pull_positions_action_config = IntegrationConfiguration.objects.create(
+        integration=provider_integration,
+        action=lotek_action_pull_positions,
+        data={"start_time": "2023-01-01T00:00:00Z"},
+    )
+
+    periodic_tasks = PeriodicTask.objects.filter(task="integrations.tasks.run_integration")
+    assert periodic_tasks.count() == 1  # Only one task should be created, for the pull_observations action
+    periodic_task = periodic_tasks[0]
+    # Configuration must be linked to the task
+    assert pull_positions_action_config.periodic_task == periodic_task
+    # Name must be shortened to 200 characters
+    assert len(periodic_task.name) == 200
+    task_parameters = json.loads(periodic_task.kwargs)
+    assert task_parameters.get("integration_id") == str(provider_integration.id)
+    assert task_parameters.get("action_id") == lotek_action_pull_positions.value
