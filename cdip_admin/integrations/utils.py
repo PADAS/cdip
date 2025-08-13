daadfc7c864c6e44e2c31e85d66e74243dd1e288
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2025-08-13 10:38:51 -0300

    Add some string cleanup for destiantion topic names

--- cdip_admin/integrations/utils.py
+++ cdip_admin/integrations/utils.py
@@ -147,7 +147,8 @@ def get_dispatcher_topic_default_name(integration, gundi_version="v2"):
         return settings.MOVEBANK_DISPATCHER_DEFAULT_TOPIC
     # Newer Connectors v2 with push data capabilities follow a naming convention
     if gundi_version == "v2":
-        return f"{integration.type.value}-push-data-topic"
+        integration_type_prefix = integration.integration.type.lower().strip().replace("_", "")
+        return f"{integration_type_prefix}-push-data-topic"
     # Fallback to legacy kafka dispatchers topic
     return f"sintegrate.observations.transformed"
 
